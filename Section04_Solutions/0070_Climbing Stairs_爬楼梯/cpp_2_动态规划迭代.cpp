0c148a27a3eb06aa2e481796eb576266e4a89b1c
kimmiGYH <crazybaofulusha@163.com>
2020-09-20 15:40:10 +0800

    DP

--- Section04_Solutions/0070_Climbing Stairs_爬楼梯/cpp_2_动态规划迭代.cpp
+++ Section04_Solutions/0070_Climbing Stairs_爬楼梯/cpp_2_动态规划迭代.cpp
@@ -7,13 +7,13 @@ class Solution {
 public:
     int climbStairs(int n) {
 
-        vector<int> memo(n+1, -1);
+        vector<int> dp(n+1, -1);
 
-        memo[0] = 1;
-        memo[1] = 1;
+        dp[0] = 1;
+        dp[1] = 1;
         for (int i = 2; i <= n; i++)
-            memo[i] = memo[i-1] + memo[i-2];
-        return memo[n];
+            dp[i] = dp[i-1] + dp[i-2];
+        return dp[n];
     }
 };
 
---
+++ Section04_Solutions/0070_Climbing Stairs_爬楼梯/cpp_3_Fibonacci滚动数组.cpp
@@ -0,0 +1,30 @@
+// 70. Climbing Stairs / Solution 3
+// Fibonacci 数列 / C++
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        int a = 1, b = 1, c = 2;
+        for (int i = 3; i <= n; ++i) {
+            a = b;
+            b = c;
+            c = a + b;
+        }
+        return c;
+    }
+};
+
+int main() {
+    Solution solution = Solution();
+    cout << solution.climbStairs(42) << endl;
+    return 0;
+}
+// Output: 433494437
