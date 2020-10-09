2effe362fd06456c372ce96f208fb451e9b15fe0
kimmiGYH <crazybaofulusha@163.com>
2020-10-09 16:14:17 +0800

    DP

--- Section02_Templates/02_二分查找/01-Binary-Search.cpp
+++ Section02_Templates/02_二分查找/01-Binary-Search.cpp
@@ -12,10 +12,33 @@ int binarySearch(T arr[], int n, T target){
     while(l <= r){    // 当 l == r时,区间[l...r]依然是有效的
         int mid = l + (r - l) / 2;
         if(arr[mid] == target) return mid;
+        
         if(target > arr[mid])
             l = mid + 1;  // target在[mid+1...r]中; [l...mid]一定没有target
         else    // target < arr[mid]
             r = mid - 1;  // target在[l...mid-1]中; [mid...r]一定没有target
+    }
+
+    return -1;
+}
+
+
+// 另种写法
+
+using namespace std;
+
+template<typename T>
+int binarySearch(T arr[], int n, T target){
+
+    int l = 0, r = n; // 在 [l...r) 的范围里寻找 target
+    while (l < r) { // 当 l == r 时，区间 [l...r) 依然是无效的
+        int mid = l + (r - l) / 2;
+        if (arr[mid] == target) return mid;
+
+        if (target > arr[mid])
+            l = mid + 1; // target在[mid+1...r)中
+        else    // target < arr[mid]
+            r = mid;    // target在[l...mid)中
     }
 
     return -1;
