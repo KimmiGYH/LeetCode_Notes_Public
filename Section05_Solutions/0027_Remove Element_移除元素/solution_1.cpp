eef6bfa5e06af485d35ff1773a48dd822e7947e3
kimmiGYH <crazybaofulusha@163.com>
2020-10-13 03:12:32 +0800

    Two Pointers

---
+++ Section05_Solutions/0027_Remove Element_移除元素/solution_1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+
+        int k = 0;
+
+        for (int i = 0; i < nums.size(); i++)
+            if (nums[i] != val)
+                nums[k++] = nums[i];
+        
+        return k;
+    }
+};
+
+int main()
+{
+    vector<int> vec = {0,1,2,2,3,0,4,2};
+    int val = 2;
+    int len = Solution().removeElement(vec, val);
+    for (int i = 0; i < len; i++)
+        cout << vec[i] << " ";
+    cout << endl;
+
+    return 0;
+}
+// 0 1 3 0 4 
+// Time: O(n); Space: O(1)
