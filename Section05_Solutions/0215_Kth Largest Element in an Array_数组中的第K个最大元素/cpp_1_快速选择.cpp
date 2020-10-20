34383b9778fcd9186dd0c1e375f33b8c1070d800
kimmiGYH <crazybaofulusha@163.com>
2020-10-20 20:57:29 +0800

    Select Sort

--- Section05_Solutions/0215_Kth Largest Element in an Array_数组中的第K个最大元素/cpp_1_快速选择.cpp
+++ Section05_Solutions/0215_Kth Largest Element in an Array_数组中的第K个最大元素/cpp_1_快速选择.cpp
@@ -6,9 +6,9 @@ using namespace std;
 class Solution {
 public:
 
-    int quick_sort(vector<int>& nums, int l, int r, int k) {
+    int quick_sort(vector<int>& nums, int l, int r, int p) {
 
-        if (l == r) return nums[k];
+        if (l == r) return nums[p];
 
         int i = l - 1, j = r + 1, x = nums[l + r >> 1];
         while (i < j) {
@@ -17,11 +17,34 @@ public:
             do j--; while (nums[j] < x);
             if (i < j) swap(nums[i], nums[j]);
         }
-        if (k <= j) return quick_sort(nums, l, j, k);
-        else return quick_sort(nums, j+1, r, k);
+        if (p <= j) return quick_sort(nums, l, j, p);
+        else return quick_sort(nums, j+1, r, p);
     }
+
 
     int findKthLargest(vector<int>& nums, int k) {
         return quick_sort(nums, 0, nums.size() - 1, k - 1);
     }
 };
+
+int main()
+{
+    vector<int> vec = {3,2,1,5,6,4,15,9,11};
+    int k = 2;
+    Solution().findKthLargest(vec, k);
+    for (int i = 0; i < vec.size(); i++)
+        cout << vec[i] << " ";
+    cout << endl;
+    cout << vec[k-1];
+
+    return 0;
+}
+
+/*
+Input:
+[3,2,1,5,6,4,15,9,11] and k = 2
+
+Output:
+15 11 9 6 5 4 1 2 3 
+11
+*/
