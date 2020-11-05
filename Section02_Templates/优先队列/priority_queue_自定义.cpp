64c85ec709964330861353a5d84d6d021f6769dc
kimmiGYH <crazybaofulusha@163.com>
2020-11-05 16:41:24 +0800

    Priority_Queue

--- Section02_Templates/优先队列/priority_queue_自定义.cpp
+++ Section02_Templates/优先队列/priority_queue_自定义.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
-#include <vector>
 #include <queue>
+#include <functional>
 #include <ctime>
 
 using namespace std;
 
 // 比较 a 和 b 的个位数
 bool myCmp(int a, int b) {
-    return a%10 < b%10;
+    return a%10 < b%10; // 个位数更大的排前面
 }
 
 int main() {
@@ -29,3 +29,17 @@ int main() {
 
     return 0;
 }
+
+/* Output:
+Insert 4 in priority queue.
+Insert 93 in priority queue.
+Insert 57 in priority queue.
+Insert 84 in priority queue.
+Insert 75 in priority queue.
+Insert 71 in priority queue.
+Insert 81 in priority queue.
+Insert 66 in priority queue.
+Insert 87 in priority queue.
+Insert 44 in priority queue.
+57 87 66 75 4 84 44 93 81 71 
+*/
