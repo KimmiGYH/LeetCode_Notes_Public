0558c2bd3fccbf7e607e13d75dd7797e3d1230a6
kimmiGYH <crazybaofulusha@163.com>
2020-11-22 13:40:04 +0800

    Stack + 表达式求值

--- Section02_Templates/表达式求值/0_Solution.md
+++ Section02_Templates/表达式求值/0_Solution.md
@@ -1,18 +1,6 @@
-# 227. Basic Calculator II_基本计算器 II (Medium)
-
-
+# 151. 表达式计算4
 
 **表达式求值问题，模板题背过即可。**
-
-
-
-- ① 如果是数，压入栈中
-- ② 如果是 `(`，压入栈中
-- ③ 如果是 `)`，则将 （`...`）括号中间的部分算完
-- ④ 如果是运算符
-  - `while` 栈顶运算符优先级 `≥` 当前运算符的优先级，则操作栈顶
-
-
 
 1. 建立一个用于存运算符的栈，逐一扫描该中缀表达式中的元素。
    1. 如果遇到一个数，输出该数。
@@ -22,4 +10,17 @@
 2. 依次取出并输出栈中的所有剩余符号，最终输出的序列就是一个与原中缀表达式等价的后缀表达式。
 
 **以上例子中包含的都是一位数，如果是多位数，并且表达式是使用字符串逐字符存储的，我们只需要稍加判断，把连续的一段数字看成一个数即可。
+
+
+
+视频：[https://www.acwing.com/video/74/](https://www.acwing.com/video/74/)
+
+![solve](https://raw.githubusercontent.com/KimmiGYH/LeetCode_Notes_Public/master/Section02_Templates/%E8%A1%A8%E8%BE%BE%E5%BC%8F%E6%B1%82%E5%80%BC/solve.png)
+
+
+
+LeetCode类似题：
+
+224. [Basic Calculator_基本计算器(Hard)](https://leetcode.com/problems/basic-calculator/) 只有 `+`、`-`，没有乘除，且不用考虑负数。
+227. [Basic Calculator II_基本计算器 II (Medium)](https://leetcode.com/problems/basic-calculator-ii/) 有 `+`、`-` 、`*`、`/`，但不用考虑括号，且不用考虑负数。
 
--- Section05_Solutions/0227_Basic Calculator II_基本计算器 II/cpp_1_stack.cpp
+++ Section05_Solutions/0227_Basic Calculator II_基本计算器 II/cpp_1_stack.cpp
@@ -9,15 +9,44 @@ public:
     stack<int> num;
     stack<char> op;
 
+    void eval()
+    {
+        int b = num.top(); num.pop();
+        int a = num.top(); num.pop();
+        char c = op.top(); op.pop();
+        int r;
+        if (c == '+')  r = a + b;
+        else if (c == '-')  r = a - b;
+        else if (c == '*')  r = a * b;
+        else r = a / b;
+        num.push(r);
+    }
+
+    
     int calculate(string s) {
         // 记录运算符的优先级
         unordered_map<char, int> priority;
         priority['+'] = priority['-'] = 1;
         priority['*'] = priority['/'] = 2;
+
         for (int i = 0; i < s.size(); i++) {
             char c = s[i];
             if (c == ' ')  continue;
+            if (isdigit(c))
+            {
+                int x = 0, j = i;
+                while (j < s.size() && isdigit(s[j]))
+                    x = x * 10 + (s[j++] - '0');
+                i = j - 1;
+                num.push(x);
+            }
+            else
+            {
+                while (op.size() && priority[op.top()] >= priority[c]) eval();
+                op.push(c);
+            }
         }
-        
+        while (op.size())  eval();
+        return num.top();
     }
 };
