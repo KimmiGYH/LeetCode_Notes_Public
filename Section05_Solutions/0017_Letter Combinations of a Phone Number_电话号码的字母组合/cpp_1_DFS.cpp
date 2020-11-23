a8219dd1191a0d4c76270a0ba25736d292cb6934
kimmiGYH <crazybaofulusha@163.com>
2020-11-23 08:20:41 +0800

    Backtracking + DFS

--- Section05_Solutions/0017_Letter Combinations of a Phone Number_电话号码的字母组合/cpp_1_DFS.cpp
+++ Section05_Solutions/0017_Letter Combinations of a Phone Number_电话号码的字母组合/cpp_1_DFS.cpp
@@ -20,6 +20,7 @@ public:
     }
 
     void dfs(string& digits, int u, string path) {
+        cout << u << " : " << path << endl;
         if (u == digits.size())  ans.push_back(path);
         else {
             for (auto c : strs[digits[u] - '0'])
@@ -27,3 +28,80 @@ public:
         }
     }
 };
+
+int main() {
+    vector<string> res = Solution().letterCombinations("234");
+    for (int i = 0; i < res.size(); i++)
+        cout << res[i] << endl;
+    return 0;
+}
+
+/*
+0 : 
+1 : a
+2 : ad
+3 : adg
+3 : adh
+3 : adi
+2 : ae
+3 : aeg
+3 : aeh
+3 : aei
+2 : af
+3 : afg
+3 : afh
+3 : afi
+1 : b
+2 : bd
+3 : bdg
+3 : bdh
+3 : bdi
+2 : be
+3 : beg
+3 : beh
+3 : bei
+2 : bf
+3 : bfg
+3 : bfh
+3 : bfi
+1 : c
+2 : cd
+3 : cdg
+3 : cdh
+3 : cdi
+2 : ce
+3 : ceg
+3 : ceh
+3 : cei
+2 : cf
+3 : cfg
+3 : cfh
+3 : cfi
+adg
+adh
+adi
+aeg
+aeh
+aei
+afg
+afh
+afi
+bdg
+bdh
+bdi
+beg
+beh
+bei
+bfg
+bfh
+bfi
+cdg
+cdh
+cdi
+ceg
+ceh
+cei
+cfg
+cfh
+cfi
+*/
