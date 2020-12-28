499cd569798a047861f090eddf75db909aa1910c
kimmiGYH <crazybaofulusha@163.com>
2020-12-28 19:32:35 +0800

    Priority Queue

--- Section05_Solutions/0347_Top K Frequent Elements_前 K 个高频元素/cpp_1_priority_queue_去批注.cpp
+++ Section05_Solutions/0347_Top K Frequent Elements_前 K 个高频元素/cpp_1_priority_queue_去批注.cpp
@@ -6,26 +6,24 @@ using namespace std;
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        // 统计每个元素出现的频率
         unordered_map<int, int> freq;
         for (int i = 0; i < nums.size(); i++)
             freq[nums[i]]++;
         
-        // 扫描 freq，维护当前出现频率最高的 k 个元素
-        // 底层应该是个最小堆，以便 pop 掉频率最低的元素
         // 比较的应该是元素出现的频率，存入 pair<元素频率，元素>
         priority_queue< pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>> > heap;
+
         // iter 访问频率表（元素，元素频率）
         for(unordered_map<int,int>::iterator iter = freq.begin(); iter != freq.end(); iter++) {
             if (heap.size() == k) {
-                if (iter->second > heap.top().first) { // 如果大于优先队列中队首所对应的频率
+                if (iter->second > heap.top().first) {
                     heap.pop();
                     heap.push(make_pair(iter->second, iter->first));
                 }
-            } else // 如果没满
+            } else
                 heap.push(make_pair(iter->second, iter->first));
         }
-        // 把优先队列中频率最高的 k 个元素放进 vector
+
         vector<int> res;
         while (!heap.empty()) {
             res.push_back(heap.top().second);
