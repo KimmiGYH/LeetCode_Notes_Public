b79f0adaf0c15c24afd0a482c0dc176731bfd457
kimmiGYH <crazybaofulusha@163.com>
2020-12-30 16:09:49 +0800

    Prefix Sum

--- Section05_Solutions/0437_Path Sum III_路径总和 III/cpp_2_前缀和.cpp
+++ Section05_Solutions/0437_Path Sum III_路径总和 III/cpp_2_前缀和.cpp
@@ -19,8 +19,8 @@ class Solution {
 public:
     int pathSum(TreeNode* root, int sum) {
       if(!root) return 0;
-      //可能存在以头结点为路径起始，所以头节点之前也要计数一次
-      hash[0] = 1; //等价于hash = {{0, 1}};
+      // 可能存在以头结点为路径起始，所以头节点之前也要计数一次
+      hash[0] = 1; // 等价于hash = {{0, 1}};
       dfs(root, sum, 0);
       return ans;
     }
@@ -29,14 +29,14 @@ private:
     unordered_map<int,int> hash;
     void dfs(TreeNode* root, int& sum, int routeSum){
         if(!root) return;
-        //路径和累加
+        // 路径和累加
         routeSum += root->val;
 
-        //将以当前节点为尾节点的等于sum（符合条件）的路径条数加到结果上
-        //因为是以当前节点为路径尾部，所以计数不会重复
+        // 将以当前节点为尾节点的等于sum（符合条件）的路径条数加到结果上
+        // 因为是以当前节点为路径尾部，所以计数不会重复
         ans += hash[routeSum - sum];
         
-        //将当前的前缀和记录下来
+        // 将当前的前缀和记录下来
         hash[routeSum]++;
         dfs(root->left, sum, routeSum);
         dfs(root->right, sum, routeSum);
