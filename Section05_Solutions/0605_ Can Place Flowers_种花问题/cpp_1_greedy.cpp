80ad076c488828ad1085eb1628b9749f87485029
kimmiGYH <crazybaofulusha@163.com>
2021-01-01 14:17:14 +0800

    Greedy

--- Section05_Solutions/0605_ Can Place Flowers_种花问题/cpp_1_greedy.cpp
+++ Section05_Solutions/0605_ Can Place Flowers_种花问题/cpp_1_greedy.cpp
@@ -5,14 +5,14 @@ using namespace std;
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        int len = flowerbed.size();
+        int len = flowerbed.size(), slots = 0;
         for (int i = 0; i < len; i++)
             if (flowerbed[i] == 0) {
                 if ((i == 0 || flowerbed[i - 1] == 0) && (i == len - 1 || flowerbed[i + 1] == 0)) {
                     flowerbed[i] = 1;
-                    n--;
+                    slots++;
                 }
             }
-        return n <= 0;
+        return slots >= n;
     }
 };
