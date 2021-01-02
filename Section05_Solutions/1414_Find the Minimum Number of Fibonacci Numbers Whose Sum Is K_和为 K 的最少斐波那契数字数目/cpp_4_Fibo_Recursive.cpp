520871ff1bc37e46727b34d3acc48d7489650d54
kimmiGYH <crazybaofulusha@163.com>
2021-01-02 17:51:00 +0800

    Fibonacci + Greedy + BinarySearch

---
+++ Section05_Solutions/1414_Find the Minimum Number of Fibonacci Numbers Whose Sum Is K_和为 K 的最少斐波那契数字数目/cpp_4_Fibo_Recursive.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int findMinFibonacciNumbers(int k) {
+    if (k == 0) return 0;
+    int a = 1, b = 1;    
+    while (b <= k) {
+      swap(a, b);
+      b += a;
+    }    
+    return 1 + findMinFibonacciNumbers(k - a);
+  }
+};
