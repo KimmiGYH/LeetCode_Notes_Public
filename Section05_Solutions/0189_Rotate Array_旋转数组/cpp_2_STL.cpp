057610fecf59c2efde97c87054fb08d006e92689
kimmiGYH <crazybaofulusha@163.com>
2021-01-08 13:45:00 +0800

    Array

---
+++ Section05_Solutions/0189_Rotate Array_旋转数组/cpp_2_STL.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k %= n;
+        for (int i = 0; i < n - k; ++i) {
+            nums.push_back(nums[0]);
+            nums.erase(nums.begin());
+        }
+    }
+};
