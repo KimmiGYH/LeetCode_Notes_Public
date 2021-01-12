389d86d89b6cc1d88a3108eae394829ccb89ea9f
kimmiGYH <crazybaofulusha@163.com>
2021-01-12 19:27:28 +0800

    DP + 完全背包

--- TempCode/0322_Coin Change_零钱兑换/cpp_1_完全背包.cpp
+++ TempCode/0322_Coin Change_零钱兑换/cpp_1_完全背包.cpp
@@ -4,17 +4,18 @@
 
 using namespace std;
 
-// 完全背包问题：f(i, j) = max{ f(i-1, j), f(i, j - v) + w };
+// 完全背包问题：f(i, j) = max( f(i-1, j), f(i, j - v) + w );
+// 完全背包一维：f[j] = min( f[j], f[j - coins[i]] + 1 )
 
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount + 1, INT_MAX);
+        vector<int> dp(amount + 1, 1e8);
         dp[0] = 0;
         for (auto v : coins)
             for (int j = v; j <= amount; j++)
                 dp[j] = min(dp[j], dp[j - v] + 1);
-        if (dp[amount] == INT_MAX) return -1;
+        if (dp[amount] == 1e8) return -1;
         return dp[amount];
     }
 };
