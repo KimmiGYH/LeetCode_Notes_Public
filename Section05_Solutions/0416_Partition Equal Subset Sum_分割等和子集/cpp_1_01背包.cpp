90826a271c9e42c549fd9c57cd0b6fd9302ef83a
kimmiGYH <crazybaofulusha@163.com>
2021-01-12 16:58:24 +0800

    DP + 01背包

--- Section05_Solutions/0416_Partition Equal Subset Sum_分割等和子集/cpp_1_01背包.cpp
+++ Section05_Solutions/0416_Partition Equal Subset Sum_分割等和子集/cpp_1_01背包.cpp
@@ -3,6 +3,10 @@
 
 using namespace std;
 
+// 状态转移方程：F(i, c) =  F(i-1, c) || F(i-1, c - w(i))
+// 如果 i-1 个物品可以填满背包，那么使用 i 个物品也可以填满背包；
+// 或者已经可以用 i-1 个物品填满 c-w(i)，对于第 i 个物品使用它，整体也填满了容量为 c 的背包。
+
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
