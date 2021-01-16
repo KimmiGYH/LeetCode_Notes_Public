3fb01c8144fc9488c93b527e24d2d3c0b85dc594
kimmiGYH <crazybaofulusha@163.com>
2021-01-16 17:46:13 +0800

    DP

--- Section05_Solutions/0062_Unique Paths_不同路径/cpp_1_DP.cpp
+++ Section05_Solutions/0062_Unique Paths_不同路径/cpp_1_DP.cpp
@@ -9,7 +9,7 @@ public:
         vector<vector<int>> dp(m, vector<int>(n));
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (!i && !j) dp[i][j] = 1;
+                if (!i && !j) dp[i][j] = 1; // 起点出发，一种方案
                 else {
                     if (i) dp[i][j] += dp[i - 1][j];
                     if (j) dp[i][j] += dp[i][j - 1];
--- Section05_Solutions/0063_Unique Paths II_不同路径 II/cpp_1_DP_v1.cpp
+++ Section05_Solutions/0063_Unique Paths II_不同路径 II/cpp_1_DP_v1.cpp
@@ -13,7 +13,7 @@ public:
 
         for (int i = 0; i < m; i++)
             for (int j = 0; j < n; j++) {
-                if (obstacleGrid[i][j] == 0) {
+                if (obstacleGrid[i][j] == 0) { // 没遇到障碍时
                     if (!i && !j) dp[i][j] = 1;
                     else {
                         if (i > 0) dp[i][j] += dp[i - 1][j];
