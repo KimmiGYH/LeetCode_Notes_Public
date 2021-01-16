b23e1113f420803341bd1c99c4e5d3eb592b1c92
kimmiGYH <crazybaofulusha@163.com>
2021-01-16 18:20:03 +0800

    DP

--- Section05_Solutions/0064_Minimum Path Sum_最小路径和/0_Solution.md
+++ Section05_Solutions/0064_Minimum Path Sum_最小路径和/0_Solution.md
@@ -1,17 +1,16 @@
 # 64. Minimum Path Sum
 
-## 方法一：动态规划
+## 方法一：动态规划 $O(m*n)$
 
-(动态规划) O(mn)
-经典的动态规划题，用`f[i][j]`记录到达网格`grid[i][j]`位置经过的最小的路径和，转移方程为`f[i][j] = grid[i][j] + min(f[i-1][j], f[i][j-1])`。
+经典的动态规划题
 
-`f[i][j]` 表示从起点到 `f[i,j]` 的所有路径的最小值；
+- 状态表示：$f(i, j)$ 表示从起点到 `(i, j)` 的所有路径和的最小值；
+  - 属性：$Min$
 
-`grid[i,j]` 表示 [i,j] 的值。
+- 转移方程：$f(i, j) = min(f(i-1, j), f(i, j-1)) + grid(i, j) $
+  - `grid[i,j]` 表示 `[i,j]` 的值。
 
-```cpp
-                    if (i)
-                        f[i][j] = min(f[i][j], f[i - 1][j] + grid[i][j]);
-                    if (j)
-                        f[i][j] = min(f[i][j], f[i][j - 1] + grid[i][j]);
+```c++
+if (i)    f[i][j] = min(f[i][j], f[i - 1][j] + grid[i][j]);
+if (j)    f[i][j] = min(f[i][j], f[i][j - 1] + grid[i][j]);
 ```
--- Section05_Solutions/0064_Minimum Path Sum_最小路径和/cpp_1_BottomUp.cpp
+++ Section05_Solutions/0064_Minimum Path Sum_最小路径和/cpp_1_BottomUp.cpp
@@ -17,23 +17,20 @@ Output: 7
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        int n = grid.size(); // 行数
-        if (!n) return 0;
-        int m = grid[0].size(); // 求列数
+        int m = grid.size(); // 行数
+        if (!m) return 0;
+        int n = grid[0].size(); // 求列数
 
-        vector<vector<int>> f(n, vector<int>(m, INT_MAX)); // 状态数组
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < m; j++) {
+        vector<vector<int>> f(m, vector<int>(n, INT_MAX)); // 状态数组
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++) {
                 if (!i && !j)
                     f[i][j] = grid[i][j]; // 如果是起点
                 else {
-                    if (i)
-                        f[i][j] = min(f[i][j], f[i - 1][j] + grid[i][j]);
-                    if (j)
-                        f[i][j] = min(f[i][j], f[i][j - 1] + grid[i][j]);
+                    if (i)  f[i][j] = min(f[i][j], f[i - 1][j] + grid[i][j]);
+                    if (j)  f[i][j] = min(f[i][j], f[i][j - 1] + grid[i][j]);
                 }
             }
-        
-        return f[n - 1][m - 1];
+        return f[m - 1][n - 1];
     }
 };
