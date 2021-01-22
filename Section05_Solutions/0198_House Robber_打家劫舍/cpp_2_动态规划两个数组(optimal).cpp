4242df46bd8ba9f1635f7a9dad172d93e7cc89a1
kimmiGYH <crazybaofulusha@163.com>
2021-01-22 20:00:13 +0800

    DP

--- Section05_Solutions/0198_House Robber_打家劫舍/cpp_2_动态规划两个数组(optimal).cpp
+++ Section05_Solutions/0198_House Robber_打家劫舍/cpp_2_动态规划两个数组(optimal).cpp
@@ -3,10 +3,12 @@
 #include <algorithm>
 using namespace std;
 
-class Solution {
+class Solution {    
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
+        // f(i) 表示 从 1~i 中选，必选 i
+        // g(i) 表示 从 1~i 中选，必不选 i
         vector<int> f(n + 1), g(n + 1);
         for (int i = 1; i <= n; i++) {
             f[i] = g[i - 1] + nums[i - 1]; // nums下标是从0开始的，所以减去1错一位
