4de8504293539921a591ab4cf88f6c9466efd52b
kimmiGYH <crazybaofulusha@163.com>
2021-01-22 14:28:09 +0800

    DP + Fibonacci

---
+++ Section05_Solutions/1137_N-th Tribonacci Number_第 N 个泰波那契数/cpp_2_变量递推.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int tribonacci(int n) {
+        if (n == 0) return 0;
+        if (n == 1 || n == 2) return 1;
+        int a = 0, b = 1, c = 1, result;
+        for (int i = 3; i <= n; i++) {
+            result = a + b + c;
+            a = b;
+            b = c;
+            c = result;            
+        }
+        return result;
+    }
+};
