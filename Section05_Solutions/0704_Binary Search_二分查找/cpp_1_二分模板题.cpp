eeb8b7789637b546021c6999823834ee7d10e4f4
kimmiGYH <crazybaofulusha@163.com>
2021-02-06 10:31:27 +0800

    Binary Search

---
+++ Section05_Solutions/0704_Binary Search_二分查找/cpp_1_二分模板题.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size();
+        while (l < r) {
+            int mid = l + r >> 1;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] > target) r = mid;
+            else l = mid + 1;
+        }
+        return -1;
+    }
+};
