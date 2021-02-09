ea59687216eca9e4992f255ab1e8ba578a233cd5
kimmiGYH <crazybaofulusha@163.com>
2021-02-09 16:42:42 +0800

    差分

---
+++ Section05_Solutions/0413_Arithmetic Slices_等差数列划分/cpp_1_差分.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& A) {
+        for (int i = A.size() - 1; i > 0; i--)  A[i] -= A[i - 1];
+        int res = 0;
+        for (int i = 1; i < A.size(); i++) {
+            int j = i; //双指针
+            while (j < A.size() && A[j] == A[i]) j++; //计算相同的差分元素有多少个，移动右指针 
+            int k = j - i; //k表示相同的差分数组元素有几个
+            res += k * (k - 1) / 2;
+            i = j - 1; //更新i到差分数组相同的最后一个元素
+        }
+        return res;
+    }
+};
