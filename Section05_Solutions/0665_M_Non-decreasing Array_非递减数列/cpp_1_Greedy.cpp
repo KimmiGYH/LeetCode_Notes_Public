a1838ef02c91a619c07b1e5ff1cd458fde70e2a0
KimmiGYH <crazybaofulusha@163.com>
2021-09-16 22:17:10 +0800

    贪心

---
+++ Section05_Solutions/0665_M_Non-decreasing Array_非递减数列/cpp_1_Greedy.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        for (int i = 1; i < nums.size(); i ++)
+            if (nums[i - 1] > nums[i])
+                return false;
+        return true;
+    }
+    
+    bool checkPossibility(vector<int>& nums) {
+        for (int i = 1; i < nums.size(); i ++)
+            if (nums[i - 1] > nums[i]) {
+                int a = nums[i - 1], b = nums[i];
+                nums[i - 1] = nums[i] = a;
+                if (check(nums))  return true;
+                nums[i - 1] = nums[i] = b;
+                if (check(nums))  return true;
+                return false;
+            }
+        return true;
+    }
+};
