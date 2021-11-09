715f5f2faf2b614a035aebd6c4307cc51d1ec05a
KimmiGYH <crazybaofulusha@163.com>
2021-11-09 10:54:58 +0800

    位运算+lowbit+数论

--- Section05_Solutions/0342_Power of Four_4 的幂/cpp_1_位运算.cpp
+++ Section05_Solutions/0342_Power of Four_4 的幂/cpp_1_位运算.cpp
@@ -2,5 +2,9 @@ class Solution {
 public:
     bool isPowerOfFour(int n) {
         return n > 0 && !(n & (n - 1)) && (n & 1431655765);
+        //lowbit
+        return n > 0 && (n & -n) == n && (n & 1431655765);
+        //取模性质，不能用 n%4==0，因为当n=8时会出问题
+        return n > 0 && !(n & (n - 1)) && n % 3 == 1;
     }
 };
