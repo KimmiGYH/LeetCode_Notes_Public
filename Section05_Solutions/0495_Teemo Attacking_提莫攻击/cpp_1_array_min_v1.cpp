1f78273af8d964e03563e2b06e75eea123a96894
KimmiGYH <crazybaofulusha@163.com>
2021-11-10 15:10:01 +0800

    数组模拟

--- Section05_Solutions/0495_Teemo Attacking_提莫攻击/cpp_1_array_min_v1.cpp
+++ Section05_Solutions/0495_Teemo Attacking_提莫攻击/cpp_1_array_min_v1.cpp
@@ -6,11 +6,12 @@ using namespace std;
 class Solution {
 public:
     int findPoisonedDuration(vector<int>& timeSeries, int duration) {
-        if (timeSeries.empty()) return 0;
-        int res = 0;
-        for (int i = 1; i < timeSeries.size(); i++)
-            res += min(timeSeries[i] - timeSeries[i - 1], duration);
-        res += duration;
+        int diff = 0, res = 0;
+        for (int i = 1; i < timeSeries.size(); i ++) {
+            diff = timeSeries[i] - timeSeries[i - 1];
+            res += min(diff, duration);
+        }
+        if (timeSeries.size()) res += duration;
         return res;
     }
 };
