f9cdd4f0ad738db98bdec39846c64ab881fb4dff
KimmiGYH <crazybaofulusha@163.com>
2021-11-17 10:10:00 +0800

    kth 二分

--- Section05_Solutions/0378_Kth Smallest Element in a Sorted Matrix_有序矩阵中第K小的元素/cpp_1_Binary Search.cpp
+++ Section05_Solutions/0378_Kth Smallest Element in a Sorted Matrix_有序矩阵中第K小的元素/cpp_1_Binary Search.cpp
@@ -11,7 +11,7 @@ public:
             int mid = (long long)l + r >> 1;
             int j = matrix[0].size() - 1, cnt = 0;
             for (int i = 0; i < matrix.size(); ++i) {
-                while (j >= 0 && mid < matrix[i][j]) j--;
+                while (j >= 0 && matrix[i][j] > mid) j--;
                 cnt += j + 1;
             }
             if (cnt >= k) r = mid;
