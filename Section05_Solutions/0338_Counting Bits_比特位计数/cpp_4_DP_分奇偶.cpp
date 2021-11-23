e648647443ea0c1da74dc2fa6caec2eb8634ebf6
KimmiGYH <crazybaofulusha@163.com>
2021-11-23 18:31:49 +0800

    位运算lowbit + DP

--- Section05_Solutions/0338_Counting Bits_比特位计数/cpp_4_DP_分奇偶.cpp
+++ Section05_Solutions/0338_Counting Bits_比特位计数/cpp_4_DP_分奇偶.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> res(n, 0);
-        res.push_back(0);
-        if (!n) return res;
+        vector<int> dp(n + 1, 0);
         for (int i = 1; i <= n; ++i) {
-            if (i % 2 == 0)
-                res[i] = res[i >> 1];
+            if (i & 1)
+                dp[i] = dp[i>>1] + 1; //或写成 dp[i] = dp[i - 1] + 1;
             else
-                res[i] = res[i >> 1] + 1;
+                dp[i] = dp[i >> 1];
         }
-        return res;
+        return dp;
+    }
+};
+
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> dp(n + 1, 0);
+        for (int i = 1; i <= n; ++i)
+            dp[i] = i & 1? dp[i-1] + 1 : dp[i>>1];
+            // 等价于 dp[i] - dp[i & (i-1)] + 1;
+        return dp;
     }
 };
