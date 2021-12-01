d12a4dbbc11394e983e750c1e06973c0b4cb88c3
KimmiGYH <crazybaofulusha@163.com>
2021-12-01 10:22:03 +0800

    差分+双指针+DP

--- Section05_Solutions/0413_Arithmetic Slices_等差数列划分/cpp_1_差分.cpp
+++ Section05_Solutions/0413_Arithmetic Slices_等差数列划分/cpp_1_差分.cpp
@@ -5,12 +5,12 @@ using namespace std;
 
 class Solution {
 public:
-    int numberOfArithmeticSlices(vector<int>& A) {
-        for (int i = A.size() - 1; i > 0; i--)  A[i] -= A[i - 1];
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        for (int i = nums.size() - 1; i > 0; i--)  nums[i] -= nums[i - 1];
         int res = 0;
-        for (int i = 1; i < A.size(); i++) {
+        for (int i = 1; i < nums.size(); i++) {
             int j = i; //双指针
-            while (j < A.size() && A[j] == A[i]) j++; //计算相同的差分元素有多少个，移动右指针 
+            while (j < nums.size() && nums[j] == nums[i]) j++; //计算相同的差分元素有多少个，移动右指针 
             int k = j - i; //k表示差分数组右端点可以取的数的个数
             res += k * (k - 1) / 2;
             i = j - 1; //更新i到差分数组相同的最后一个元素
---
+++ Section05_Solutions/0413_Arithmetic Slices_等差数列划分/cpp_2_DP.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        int res = 0, n = nums.size();
+        vector<int> dp(n, 0);
+        for (int i = 2; i < n; ++i) {
+            if (nums[i] - nums[i-1] == nums[i-1] - nums[i-2])
+                dp[i] = dp[i-1] + 1;
+            res += dp[i];
+        }
+        return res;
+    }
+};
