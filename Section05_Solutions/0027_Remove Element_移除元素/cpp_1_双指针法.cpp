3fefee7c61d27cc8f5b182581fcbfd96b616482d
KimmiGYH <crazybaofulusha@163.com>
2021-12-29 18:58:46 +0800

    双指针

--- Section05_Solutions/0027_Remove Element_移除元素/cpp_1_双指针法.cpp
+++ Section05_Solutions/0027_Remove Element_移除元素/cpp_1_双指针法.cpp
@@ -5,7 +5,6 @@ using namespace std;
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-
         int k = 0;
         for (int i = 0; i < nums.size(); ++i)
             if (nums[i] != val)
