39027a760535546ccf6353e386446fa0cb309ff9
KimmiGYH <crazybaofulusha@163.com>
2021-12-30 11:28:12 +0800

    贪心

---
+++ Section05_Solutions/1221_Split a String in Balanced Strings_分割平衡字符串/cpp_1_贪心.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int res = 0, cnt = 0;
+        for (char& c : s) {
+            (c == 'L') ? ++cnt : --cnt;
+            if (cnt == 0) ++res;
+        }
+        return res;
+    }
+};
