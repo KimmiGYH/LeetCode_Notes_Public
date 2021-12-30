103a5691664eda53dbd2ab4325b7c74971aea631
KimmiGYH <crazybaofulusha@163.com>
2021-12-30 10:55:45 +0800

    哈希表+双指针

--- Section05_Solutions/0594_Longest Harmonious Subsequence_最长和谐子序列/cpp_2_排序双指针.cpp
+++ Section05_Solutions/0594_Longest Harmonious Subsequence_最长和谐子序列/cpp_2_排序双指针.cpp
@@ -3,10 +3,8 @@ public:
     int findLHS(vector<int>& nums) {
         int res = 0;
         sort(nums.begin(), nums.end());
-        int i = 0, j = 0;
-        for (j = 0; j < nums.size(); j++) {
-            while (nums[j] - nums[i] > 1)
-                i++;
+        for (int i = 0, j = 0; j < nums.size(); j++) {
+            while (nums[j] - nums[i] > 1) i++;
             if (nums[j] - nums[i] == 1)
                 res = max(res, j - i + 1);
         }
