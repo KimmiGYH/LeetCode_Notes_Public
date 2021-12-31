97c450251c051e62937c22cba23c944f61f8dc13
KimmiGYH <crazybaofulusha@163.com>
2021-12-31 14:09:54 +0800

    双指针

--- Section05_Solutions/0015_3Sum_三数之和/cpp_1_双指针.cpp
+++ Section05_Solutions/0015_3Sum_三数之和/cpp_1_双指针.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
         vector<vector<int>> res;
         sort(nums.begin(), nums.end());
-        for (int i = 0; i < nums.size(); i++) {
-            if (i > 0 && nums[i] == nums[i-1]) continue;
-            for (int j = i+1, k = nums.size() - 1; j < k; j++) {
-                if (j > i+1 && nums[j] == nums[j-1]) continue;
-                while (j < k-1 && nums[i] + nums[j] + nums[k] > 0) k--;
+        for (int i = 0; i <= n-1; i++) {
+            if (i >= 1 && nums[i] == nums[i-1]) continue;
+            for (int j = i+1, k = n-1; j < k; j++) {
+                if (j >= i+2 && nums[j] == nums[j-1]) continue;
+                while (j <= k-2 && nums[i] + nums[j] + nums[k] > 0) k--;
                 if (nums[i] + nums[j] + nums[k] == 0)
-                    res.push_back({nums[i], nums[j], nums[k]});
+                    res.push_back({nums[k], nums[i], nums[j]});
             }
         }
         return res;
