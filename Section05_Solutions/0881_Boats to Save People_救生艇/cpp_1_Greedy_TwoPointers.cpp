fafb861b84da0c0bb34407a890b7467920d64e50
KimmiGYH <crazybaofulusha@163.com>
2022-01-01 13:17:58 +0800

    z双指针+贪心

--- Section05_Solutions/0881_Boats to Save People_救生艇/cpp_1_Greedy_TwoPointers.cpp
+++ Section05_Solutions/0881_Boats to Save People_救生艇/cpp_1_Greedy_TwoPointers.cpp
@@ -7,11 +7,11 @@ using namespace std;
 class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
-        int res = 0, n = people.size(), left = 0, right = n - 1;
-        sort(people.begin(), people.end()); //left 瘦子, right 胖子
-        while (left <= right) {
-            if (people[left] + people[right] <= limit) left++;
-            right--;
+        int res = 0, n = people.size(), l = 0, r = n - 1;
+        sort(people.begin(), people.end()); //l 瘦子, r 胖子
+        while (l <= r) {
+            if (people[l] + people[r] <= limit) l++;
+            r--;
             res++;
         }
         return res;        
