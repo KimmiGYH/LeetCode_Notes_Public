b053a0c2b75b5ccb7ee3b3bac88a0d8b0fd9ff78
KimmiGYH <crazybaofulusha@163.com>
2022-01-01 13:43:25 +0800

    双指针+贪心

--- Section05_Solutions/0881_Boats to Save People_救生艇/cpp_1_Greedy_TwoPointers.cpp
+++ Section05_Solutions/0881_Boats to Save People_救生艇/cpp_1_Greedy_TwoPointers.cpp
@@ -20,3 +20,15 @@ public:
 
 // ---------------------------------------
 
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(), people.end());
+        int n = people.size(), res = 0;
+        for (int i = 0, j = n-1; i <= j; j--) {
+            if (people[i] + people[j] <= limit) i++;
+            res++;
+        }
+        return res;
+    }
+};
