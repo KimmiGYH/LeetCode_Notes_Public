90ef01c2e1557137c286f26b50b953658aa54988
KimmiGYH <crazybaofulusha@163.com>
2022-01-03 11:41:46 +0800

    双指针+滑动窗口

--- Section05_Solutions/0438_Find All Anagrams in a String_找到字符串中所有字母异位词/cpp_1_分开数组.cpp
+++ Section05_Solutions/0438_Find All Anagrams in a String_找到字符串中所有字母异位词/cpp_1_分开数组.cpp
@@ -6,16 +6,13 @@ using namespace std;
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        int n = s.size();
-        int l = p.size();
-        vector<int> res;
-        vector<int> vp(26, 0);
-        vector<int> vs(26, 0);
-        for (char ch : p) ++vp[ch - 'a'];
+        int n = s.size(), m = p.size();
+        vector<int> res, vs(26), vp(26);
+        for (char& c : p) ++vp[c - 'a'];
         for (int i = 0; i < n; ++i) {
             ++vs[s[i] - 'a'];
-            if (i >= l)  --vs[s[i-l] - 'a'];
-            if (vs == vp)  res.push_back(i + 1 - l);
+            if (i >= m) --vs[s[i-m] - 'a'];
+            if (vs == vp) res.push_back(i - m + 1);
         }
         return res;
     }
--- Section05_Solutions/0438_Find All Anagrams in a String_找到字符串中所有字母异位词/cpp_1_合并哈希表 + 滑动窗口.cpp
+++ Section05_Solutions/0438_Find All Anagrams in a String_找到字符串中所有字母异位词/cpp_1_合并哈希表 + 滑动窗口.cpp
@@ -5,19 +5,18 @@ using namespace std;
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        unordered_map<char, int> cnt;
-        for (auto c : p)  cnt[c]++;
+        unordered_map<char, int> hash;
         vector<int> res;
-        int tot = cnt.size();
-        for (int i = 0, j = 0, satisfy = 0; i < s.size(); i++) {
-            // 左边每出现一个字符时 ++，右边每出现一个字符时 --
-            if (--cnt[s[i]] == 0)  satisfy++;
-            // 因为是 -- 等于0，所以减之前必然大于0，大于0则表示当前字符在目标串里出现过
-            while (i - j + 1 > p.size()) { // 区间大于目标串长度时 要删字符
-                if (cnt[s[j]] == 0)  satisfy--;
-                cnt[s[j++]]++;
+
+        for (auto& c : p)  hash[c]++;
+        int tot = hash.size();
+        for (int i = 0, j = 0, satisfy = 0; j < s.size(); j++) {
+            if (--hash[s[j]] == 0)  satisfy++;//每出现一个目标串中的字符就抵消1次，如果该字符被抵消为0，说明某个字符与目标串中的字符出现次数相同，匹配字符数加1
+            while (j - i + 1 > p.size()) { //区间大于目标串长度时，要删字符
+                if (hash[s[i]] == 0)  satisfy--; //如果删之前s[i]这个字符已被抵消为0了，删完就被恢复了一次，所以satisfy--，匹配字符数减1
+                hash[s[i++]]++; //删之前是被抵消为0了，由于共用一个哈希表，所以删之后hash[s[i]]++表示该字母s[i]已被恢复，还需要匹配一次来抵消
             }
-            if (satisfy == tot)  res.push_back(j);
+            if (satisfy == tot)  res.push_back(i);
         }
         return res;
     }
