d72a047b018cb14aed2e71a2e2c8889859e8d7d9
KimmiGYH <crazybaofulusha@163.com>
2022-01-05 12:01:29 +0800

    滑动窗口

--- Section05_Solutions/0643_Maximum Average Subarray I_子数组最大平均数/cpp_1_滑动窗口.cpp
+++ Section05_Solutions/0643_Maximum Average Subarray I_子数组最大平均数/cpp_1_滑动窗口.cpp
@@ -1,18 +1,11 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-
 class Solution {
 public:
     double findMaxAverage(vector<int>& nums, int k) {
-        double res = -1e5;
-        for (int i = 0, j = 0, s = 0; i < nums.size(); ++i)
-        {
-            s += nums[i];
-            if (i - j + 1 > k) s -= nums[j++];
-            if (i >= k - 1) res = max(res, s / double(k));
+        double res = INT_MIN;
+        for (int i = 0, j = 0, sum = 0; j < nums.size(); j++) {
+            sum += nums[j];
+            if (j - i + 1 > k) sum -= nums[i++];
+            if (j >= k - 1) res = max(res, (double)sum / k);
         }
         return res;
     }
