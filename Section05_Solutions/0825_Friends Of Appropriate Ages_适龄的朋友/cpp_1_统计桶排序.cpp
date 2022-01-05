429c8243ac7efe8e4177a8c6420367c933c1ab33
KimmiGYH <crazybaofulusha@163.com>
2022-01-05 11:31:30 +0800

    双指针+桶排序

--- Section05_Solutions/0825_Friends Of Appropriate Ages_适龄的朋友/cpp_1_统计桶排序.cpp
+++ Section05_Solutions/0825_Friends Of Appropriate Ages_适龄的朋友/cpp_1_统计桶排序.cpp
@@ -3,8 +3,7 @@ public:
     int numFriendRequests(vector<int>& ages) {
         int n = ages.size();
         int s[121] = {0};
-        for (int i =0; i < n; i++)
-            s[ages[i]]++;
+        for (int& age : ages) s[age]++;
         int res = n * n;
         for (int i = 1; i <= 120; i++)
             for (int j = 1; j <= 120; j++)
