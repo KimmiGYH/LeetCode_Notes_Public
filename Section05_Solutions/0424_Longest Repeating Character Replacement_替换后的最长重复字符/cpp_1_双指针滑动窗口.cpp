4de8fb1c4f9517cdfc6d9bd90d0702a39cac2ddd
KimmiGYH <crazybaofulusha@163.com>
2022-01-08 14:47:51 +0800

    滑动窗口

--- Section05_Solutions/0424_Longest Repeating Character Replacement_替换后的最长重复字符/cpp_1_双指针滑动窗口.cpp
+++ Section05_Solutions/0424_Longest Repeating Character Replacement_替换后的最长重复字符/cpp_1_双指针滑动窗口.cpp
@@ -5,7 +5,7 @@ public:
         for (char c = 'A'; c <= 'Z'; c++) {
             for (int i = 0, j = 0, cnt = 0; j < s.size(); j++) {//cnt当前窗口里c出现的字符次数
                 if (s[j] == c) cnt++; //判断新加字符==c的话，cnt++
-                while (j - i + 1 - cnt > k) { //总长度-cnt>k，就要删掉窗口开头的字符
+                while (j - i + 1 - cnt > k) { //总长度-cnt>k，即其他字符>k，就要删掉窗口开头的字符
                     if (s[i] == c) cnt --; //如果窗口开头的字符==c，cnt--
                     i++; //窗口左边指针右移
                 }
