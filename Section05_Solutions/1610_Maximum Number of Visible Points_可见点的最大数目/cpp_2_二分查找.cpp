03f6d91af1992c9e0d92fa9f6d76e213bd6a8711
KimmiGYH <crazybaofulusha@163.com>
2022-01-10 20:48:16 +0800

    滑动窗口+二分查找

---
+++ Section05_Solutions/1610_Maximum Number of Visible Points_可见点的最大数目/cpp_2_二分查找.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int visiblePoints(vector<vector<int>>& points, int angle, vector<int>& loc) {
+        int origin = 0, cnt = 0;
+        vector<double> list;//夹角弧度数组
+        
+        for (vector<int>& p : points) {
+            if (p[0] == loc[0] && p[1] == loc[1]) origin++;
+            else list.push_back(atan2(p[1] - loc[1], p[0] - loc[0]));
+        }
+        
+        sort(list.begin(), list.end());
+        int m = list.size();
+        for (int i = 0; i < m; i++)
+            list.push_back(list[i] + 2 * M_PI);
+        
+        double degree = angle * M_PI / 180;
+        // for (int l = 0, r = 0; r < list.size(); r++) {
+        //     while (list[r] - list[l] > degree) l++;
+        //     cnt = max(cnt, r - l + 1);
+        // }
+        
+        for (int i = 0; i < list.size(); i++) {
+            auto it = upper_bound(list.begin() + i, list.end(), list[i] + degree);//超出视角范围的第一个数
+            int cur = it - list.begin() - i;
+            cnt = max(cnt, cur);
+        }
+            
+        return origin + cnt;
+    }
+};
