c9d9ae6684576659bb44eb9b23f64d7e0143c3bb
KimmiGYH <crazybaofulusha@163.com>
2022-01-13 07:55:14 +0800

    二分の二段性

--- Section05_Solutions/0033_Search in Rotated Sorted Array_搜索旋转排序数组/cpp_1_分段二分.cpp
+++ Section05_Solutions/0033_Search in Rotated Sorted Array_搜索旋转排序数组/cpp_1_分段二分.cpp
@@ -1,31 +1,23 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int l = 0, r = nums.size() - 1;
+        int n = nums.size();
+        if (!n) return -1;
+        int l = 0, r = n - 1;
         while (l < r) {
-            int mid = l + r + 1 >> 1;
-            if (nums[mid] >= nums[0]) l = mid; // 找到大于nums[0]的最后一个数的下标
+            int mid = l + r + 1>> 1;
+            if (nums[mid] >= nums[0]) l = mid;
             else r = mid - 1;
         }
-        // cout << "l: " << l << " " << "r: " << r << endl; // l: 3 r: 3
         
-        if (target >= nums[0]) l = 0; // r 是上面二分之后的 r
-        else l = r + 1, r = nums.size() - 1;  // 如果target < nums[0]，就在下半段找
-        // cout << "l: " << l << " " << "r: " << r << endl; // l: 4 r: 6
-        
+        if (target >= nums[0]) l = 0;
+        else l = r + 1, r = n - 1;
         while (l < r) {
             int mid = l + r >> 1;
-            if (nums[mid] >= target) r = mid; // 找到第一个数大于等于target的数的下标
+            if (nums[mid] >= target) r = mid;
             else l = mid + 1;
         }
-        // cout << "l: " << l << " " << "r: " << r << endl; // l: 4 r: 4
         
-        if (nums[r] == target) return r;
-        return -1;
+        return nums[r] == target ? r : -1;
     }
 };
