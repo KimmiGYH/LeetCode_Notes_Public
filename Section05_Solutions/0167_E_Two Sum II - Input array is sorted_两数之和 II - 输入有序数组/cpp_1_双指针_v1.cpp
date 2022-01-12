6a163a9643826e553ca9ab862a8999d703f8c7be
KimmiGYH <crazybaofulusha@163.com>
2022-01-13 06:31:05 +0800

    双指针+二分

--- Section05_Solutions/0167_E_Two Sum II - Input array is sorted_两数之和 II - 输入有序数组/cpp_1_双指针_v1.cpp
+++ Section05_Solutions/0167_E_Two Sum II - Input array is sorted_两数之和 II - 输入有序数组/cpp_1_双指针_v1.cpp
@@ -1,8 +1,3 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
@@ -14,5 +9,37 @@ public:
             else -- r;
         }
         return vector<int>{l + 1, r + 1}
+    }
+};
+
+// ----------------------------------------------------
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int l = 0, r = numbers.size() - 1;
+        while (l < r) {
+            int sum = numbers[l] + numbers[r];
+            if (sum == target) return {l + 1, r + 1};
+            else if (sum < target) ++ l;
+            else -- r;
+        }
+        return {};
+    }
+};
+
+// ----------------------------------------------------
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int i = 0, j = numbers.size() - 1;
+        while (i < j) {
+            int sum = numbers[i] + numbers[j];
+            if (sum == target) return {i + 1, j + 1};
+            if (sum < target) i++;
+            if (sum > target) j--;
+        }
+        return {};
     }
 };
