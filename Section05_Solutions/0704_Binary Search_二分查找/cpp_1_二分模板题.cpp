f20219db7bde227cfe5e5a479733dd7e0dc8cf2f
KimmiGYH <crazybaofulusha@163.com>
2022-01-13 15:12:23 +0800

    二分模板题

--- Section05_Solutions/0704_Binary Search_二分查找/cpp_1_二分模板题.cpp
+++ Section05_Solutions/0704_Binary Search_二分查找/cpp_1_二分模板题.cpp
@@ -16,3 +16,18 @@ public:
         return -1;
     }
 };
+
+// ----------------------------------------------
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+        while (l < r) {
+            int mid = l + r >> 1;
+            if (nums[mid] >= target) r = mid;
+            else l = mid + 1;
+        }
+        return nums[r] == target ? r : -1;
+    }
+};
