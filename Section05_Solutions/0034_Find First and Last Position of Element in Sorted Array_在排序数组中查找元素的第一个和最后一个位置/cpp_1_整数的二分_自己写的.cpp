92085deefd5e6e4aae4250dd60782fe5cd7e9935
KimmiGYH <crazybaofulusha@163.com>
2022-01-15 01:24:48 +0800

    二分查找

---
+++ Section05_Solutions/0034_Find First and Last Position of Element in Sorted Array_在排序数组中查找元素的第一个和最后一个位置/cpp_1_整数的二分_自己写的.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        if (nums.empty()) return {-1, -1};
+        
+        vector<int> res;
+        int idx_left = 0, idx_right = 0;
+        
+        int l = 0, r = nums.size() - 1;
+        while (l < r) {
+            int mid = l + r >> 1;
+            if (nums[mid] >= target) r = mid;
+            else l = mid + 1;
+        }
+        idx_left = nums[r] == target ? r : -1;
+        res.push_back(idx_left);
+        
+        l = 0, r = nums.size() - 1;
+        while (l < r) {
+            int mid = l + r + 1 >> 1;
+            if (nums[mid] <= target) l = mid;
+            else r = mid - 1;
+        }
+        idx_right = nums[r] == target ? r : -1;
+        res.push_back(idx_right);
+        
+        return res;
+    }
+};
