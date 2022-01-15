7e55bbb66de82ac1d45436ccebc4f9fb51359c6a
KimmiGYH <crazybaofulusha@163.com>
2022-01-16 06:37:27 +0800

    自定义排序+优先队列+二分查找+双指针

--- Section05_Solutions/0786_K-th Smallest Prime Fraction_第 K 个最小的素数分数/cpp_3_二分查找+双指针.cpp
+++ Section05_Solutions/0786_K-th Smallest Prime Fraction_第 K 个最小的素数分数/cpp_3_二分查找+双指针.cpp
@@ -18,7 +18,7 @@ public:
     
     vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
         double l = 0, r = 1;
-        while (r - l >= eps) { //写成r - l > eps也可以
+        while (r - l >= eps) { //写成 r - l > eps 也可以
             double mid = (l + r) / 2;
             if (get(arr, mid) >= k) r = mid; //≤x 的个数如果 ≥k，x 猜太大了，要减少它的值，答案在左半边，r=mid
             else l = mid; //≤x 的个数如果＜k，x 猜太小了，要增加它的值，答案在右半边，l=mid
