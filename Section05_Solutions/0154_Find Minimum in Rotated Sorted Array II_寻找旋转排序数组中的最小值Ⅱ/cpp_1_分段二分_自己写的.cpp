8a9a99052c2b656781a7e9e6f6f6a5c88a6f7f31
KimmiGYH <crazybaofulusha@163.com>
2022-01-18 04:22:50 +0800

    二分+分段性

---
+++ Section05_Solutions/0154_Find Minimum in Rotated Sorted Array II_寻找旋转排序数组中的最小值Ⅱ/cpp_1_分段二分_自己写的.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int R = nums.size() - 1;
+        while (R > 0 && nums[R] == nums[0]) R--;
+        if (R == 0) return nums[0];
+        if (nums[R] > nums[0]) return nums[0];
+
+        int l = 0, r = R;
+        while (l < r) {
+            int mid = l + r >> 1;
+            if (nums[mid] < nums[0]) r = mid;
+            else l = mid + 1;
+        }
+        return nums[r];
+    }
+};
