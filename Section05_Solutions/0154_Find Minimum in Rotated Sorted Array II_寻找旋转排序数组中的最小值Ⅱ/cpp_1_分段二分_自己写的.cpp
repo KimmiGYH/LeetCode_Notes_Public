bb131dbe6eb356834d7115b8efbf8a96211bfc39
KimmiGYH <crazybaofulusha@163.com>
2022-01-18 20:22:35 +0800

    二分

--- Section05_Solutions/0154_Find Minimum in Rotated Sorted Array II_寻找旋转排序数组中的最小值Ⅱ/cpp_1_分段二分_自己写的.cpp
+++ Section05_Solutions/0154_Find Minimum in Rotated Sorted Array II_寻找旋转排序数组中的最小值Ⅱ/cpp_1_分段二分_自己写的.cpp
@@ -4,7 +4,7 @@ public:
         int R = nums.size() - 1;
         while (R > 0 && nums[R] == nums[0]) R--;
         // if (R == 0) return nums[0];
-        if (nums[R] > nums[0]) return nums[0];
+        if (nums[R] >= nums[0]) return nums[0];
 
         int l = 0, r = R;
         while (l < r) {
