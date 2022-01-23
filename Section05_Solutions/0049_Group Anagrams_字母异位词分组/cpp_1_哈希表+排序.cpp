fd7ef8f1c18e955156f1b297f7c4ecc2b296e809
KimmiGYH <crazybaofulusha@163.com>
2022-01-23 13:59:23 +0800

    Anagram+哈希表+排序+数组映射

---
+++ Section05_Solutions/0049_Group Anagrams_字母异位词分组/cpp_1_哈希表+排序.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> hash;
+        vector<vector<string>> res;
+        for (string& str : strs) {
+            string s = str;
+            sort(s.begin(), s.end());
+            hash[s].push_back(str);
+        }
+        for (auto& [k, v] : hash)   //for (auto& item : hash)
+            res.push_back(v);       //  res.push_back(item.second);
+        return res;
+    }
+};
+
+// ---------------------------------------------------------------
+// 另外的写法
+
+        for (auto it = hash.begin(); it != hash.end(); ++it) {
+                    ans.emplace_back(it->second);
+                }
