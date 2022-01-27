f51c9090f0a898b8aa0281b1ca281258de4a8536
KimmiGYH <crazybaofulusha@163.com>
2022-01-27 16:04:35 +0800

    逆波兰表达式/计算器

--- Section05_Solutions/0150_Evaluate Reverse Polish Notation_逆波兰表达式求值/cpp_1_stack.cpp
+++ Section05_Solutions/0150_Evaluate Reverse Polish Notation_逆波兰表达式求值/cpp_1_stack.cpp
@@ -8,16 +8,18 @@ class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
         stack<int> stk;
-        for (auto& s : tokens)
+        for (string& s : tokens) {
             if (s == "+" || s == "-" || s == "*" || s == "/") {
-                auto b = stk.top(); stk.pop();
-                auto a = stk.top(); stk.pop();
-                if (s == "+")  a += b;
-                else if (s == "-")  a -= b;
-                else if (s == "*")  a *= b;
-                else a /= b;
-                stk.push(a);
-            } else stk.push(stoi(s)); // stoi: string to int
+                int a = stk.top(); stk.pop();
+                int b = stk.top(); stk.pop();
+                if (s == "+")       b += a;
+                else if (s == "-")  b -= a;
+                else if (s == "*")  b *= a;
+                else                b /= a;
+                stk.push(b);
+            }
+            else stk.push(stoi(s));// stoi: string to int
+        }
         return stk.top();
     }
 };
