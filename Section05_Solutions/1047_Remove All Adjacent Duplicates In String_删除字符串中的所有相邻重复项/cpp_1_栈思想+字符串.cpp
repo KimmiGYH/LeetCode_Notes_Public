0cee48f5cfdc600071889fc1e148ca28932ac4f0
KimmiGYH <crazybaofulusha@163.com>
2022-01-27 14:58:44 +0800

    字符串+栈思想

---
+++ Section05_Solutions/1047_Remove All Adjacent Duplicates In String_删除字符串中的所有相邻重复项/cpp_1_栈思想+字符串.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string res;
+        for (char& c : s) {
+            if (res.empty() || res.back() != c)
+                res.push_back(c);
+            else res.pop_back();
+        }
+        return res;
+    }
+};
+
+// --------------------------------------------
+
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string res;
+        for (char& c : s) {
+            if (!res.empty() && res.back() == c)
+                res.pop_back();
+            else res.push_back(c);
+        }
+        return res;
+    }
+};
