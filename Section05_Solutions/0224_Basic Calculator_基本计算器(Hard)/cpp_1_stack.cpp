35c3cc7d43070e71825846c233abf10d6bea0206
KimmiGYH <crazybaofulusha@163.com>
2022-01-27 23:51:28 +0800

    栈+表达式求值+加减乘除/空格/特殊符号

--- Section05_Solutions/0224_Basic Calculator_基本计算器(Hard)/cpp_1_stack.cpp
+++ Section05_Solutions/0224_Basic Calculator_基本计算器(Hard)/cpp_1_stack.cpp
@@ -1,43 +1,45 @@
-#include <iostream>
-#include <string>
-#include <stack>
-using namespace std;
-
 class Solution {
 public:
     void eval(stack<int>& num, stack<char>& op) {
-        auto b = num.top(); num.pop();
-        auto a = num.top(); num.pop();
-        auto c = op.top(); op.pop();
-        int r;
-        if (c == '+')  r = a + b;
-        else r = a - b;
-        num.push(r);
+        int b = num.top(); num.pop();
+        int a = num.top(); num.pop();
+        char c = op.top(); op.pop();
+        int x = 0;
+        if (c == '+')       x = a + b;
+        else if (c == '-')  x = a - b;
+        // else if (c == '*')  x = a * b;
+        // else                x = a / b;
+        num.push(x);
     }
-
+    
     int calculate(string s) {
         stack<int> num;
         stack<char> op;
+        unordered_map<char, int> pr;
+        pr['+'] = pr['-'] = 1;
+        pr['*'] = pr['/'] = 2;
         for (int i = 0; i < s.size(); i++) {
-            auto c = s[i];
-            if (c == ' ')  continue;
+            char c = s[i];
+            if (c == ' ') continue;
             if (isdigit(c)) {
                 int x = 0, j = i;
                 while (j < s.size() && isdigit(s[j]))
-                    x = x * 10 + (s[j++] - '0'); // 加括号防止溢出
-                i = j - 1;
+                    x = x * 10 + (s[j++] - '0');
                 num.push(x);
-            } else if (c == '(') {
-                op.push(c);
-            } else if (c == ')') {
-                while (op.top() != '(')  eval(num, op);
+                i = j - 1;
+            }
+            else if (c == '(') op.push(c);
+            else if (c == ')') {
+                while (op.top() != '(') eval(num, op);
                 op.pop();
             } else {
-                while (op.size() && op.top() != '(')  eval(num, op);
+                if (!i || s[i-1] == '(' || s[i-1] == '+' || s[i-1] == '-')
+                    num.push(0);// 特殊处理符号和正号
+                while (op.size() && op.top() != '(') eval(num, op);
                 op.push(c);
             }
         }
-        while (op.size())  eval(num, op);
+        while (op.size()) eval(num, op);
         return num.top();
     }
 };
