6a15bdede13bf29c2adc26d8f089571695ec1ca2
KimmiGYH <crazybaofulusha@163.com>
2022-02-04 12:26:58 +0800

    字符串括号+贪心

--- Section05_Solutions/0921_Minimum Add to Make Parentheses Valid_使括号有效的最少添加/cpp_1_字符串括号+贪心.cpp
+++ Section05_Solutions/0921_Minimum Add to Make Parentheses Valid_使括号有效的最少添加/cpp_1_字符串括号+贪心.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-        int insert = 0, right = 0;
+        int insert = 0, need = 0;
         for (char& c : s) {
             if (c == '(') {
-                right ++;
+                need ++;
             } else if (c == ')') {
-                right --;
-                if (right == -1) {
-                    right = 0;
+                need --;
+                if (need == -1) {
                     insert ++;
+                    need = 0;
                 }
             }
         }
-        if (right > 0) insert += right;
-        return insert;
+        return insert + need;
     }
 };
 // https://leetcode-cn.com/problems/minimum-add-to-make-parentheses-valid/solution/shi-gua-hao-you-xiao-de-zui-xiao-tian-ji-18hh/
