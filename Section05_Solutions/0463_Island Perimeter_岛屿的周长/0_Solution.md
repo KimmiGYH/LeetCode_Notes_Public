79d5746bb5ab1be6dfd32d462869475163395c09
KimmiGYH <crazybaofulusha@163.com>
2022-02-23 11:51:20 +0800

    方向数组

--- Section05_Solutions/0463_Island Perimeter_岛屿的周长/0_Solution.md
+++ Section05_Solutions/0463_Island Perimeter_岛屿的周长/0_Solution.md
@@ -1,26 +1,24 @@
 # 463. Island Perimeter_岛屿的周长 (Easy)
 
-[Carl的题解](https://leetcode-cn.com/problems/island-perimeter/solution/463-dao-yu-de-zhou-chang-mo-ni-bian-ke-by-carlsun-/)
-
-
-
 ## 思路
 
 岛屿问题最容易让人想到BFS或者DFS，但是这道题还真的没有必要。
 
-
-
 ## 解法一：
 
-使用方向数组。
+- 使用方向数组。
+- 如何统计边界？从某个陆地格子出发：
+  - 1) 出界时说明是边界
+  - 2) 从陆地格子 `grid[i][j] == 1` 移动到隔壁的水域格子 `grid[x][y] == 0` 时，说明是边界
 
-遍历每一个空格，遇到岛屿，计算其上下左右的情况，遇到水域或者出界的情况，就可以计算边了。
-
-![solve_1](https://pic.leetcode-cn.com/1604025390-NccJlv-463.%E5%B2%9B%E5%B1%BF%E7%9A%84%E5%91%A8%E9%95%BF.png)
-
+**复杂度分析**
+- 时间复杂度：$O(m*n)$，其中 $m$ 为网格的高度，$n$ 为网格的宽度。我们需要遍历每个格子，每个格子要看其周围 $4$ 个格子是否为岛屿，因此总时间复杂度为 $O(4mn)=O(mn)$。
+- 空间复杂度：$O(1)$。只需要常数空间存放若干变量。
 
 
 ## 解法二：
+
+[Carl的题解](https://leetcode-cn.com/problems/island-perimeter/solution/463-dao-yu-de-zhou-chang-mo-ni-bian-ke-by-carlsun-/)
 
 计算出总的岛屿数量，因为有一对相邻两个陆地，边的总数就减 `2`，那么在计算出相邻岛屿的数量就可以了。
 
--- Section05_Solutions/0463_Island Perimeter_岛屿的周长/cpp_1_方向数组.cpp
+++ Section05_Solutions/0463_Island Perimeter_岛屿的周长/cpp_1_方向数组.cpp
@@ -1,19 +1,18 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
 class Solution {
 public:
     int islandPerimeter(vector<vector<int>>& grid) {
-        int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, 1, 0, -1};
+        int m = grid.size(), n = grid[0].size();
         int res = 0;
-        for (int i = 0; i < grid.size(); i++)
-            for (int j = 0; j < grid[i].size(); j++)
-                for (int k = 0; k < 4; k++) {
-                    int x = i + dx[k], y = j + dy[k];
-                    if (x < 0 || x >= grid.size() || y < 0 || y >= grid[0].size()) res++;
-                    else if (grid[x][y] == 0) res++;
+        int dx[4] = {-1, 0, 1, 0};
+        int dy[4] = {0, 1, 0, -1};
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                if (grid[i][j] == 1) {
+                    for (int k = 0; k < 4; k++) {
+                        int x = i + dx[k], y = j + dy[k];
+                        if (x < 0 || x >= m || y < 0 || y >= n || grid[x][y] == 0)
+                            res++;
+                    }
                 }
         return res;
     }
