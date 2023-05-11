529de781ccecf841ddc332b54f45fc249ac2727a
KimmiGYH <crazybaofulusha@163.com>
2023-05-11 16:29:38 +0800

    二分

--- Section05_Solutions/0034_Find First and Last Position of Element in Sorted Array_在排序数组中查找元素的第一个和最后一个位置/cpp_2_lower_bound().cpp
+++ Section05_Solutions/0034_Find First and Last Position of Element in Sorted Array_在排序数组中查找元素的第一个和最后一个位置/cpp_2_lower_bound().cpp
@@ -72,16 +72,18 @@ public:
         return {start, end};
     }
 };
-/* Runtime Error
-Line 1034: Char 34: runtime error: applying non-zero offset 18446744073709551612 to null pointer (stl_vector.h)
-SUMMARY: UndefinedBehaviorSanitizer: undefined-behavior /usr/bin/../lib/gcc/x86_64-linux-gnu/9/../../../../include/c++/9/bits/stl_vector.h:1043:34 */
+/* Runtime Error错误，解决方案：应该补上判空 nums.empty()
+nums=[1], target=1, 正确结果: [0,0]，但运行结果却是[0,-1],错误。解决方案：加了行 if (nums.size() == 1) return {0, 0}; 但还是不对，遇到另外情况：
+nums=[2,2], target=2, 正确结果：[0,1]，但运行结果却是[0,0],错误。解决方案：分开来单独判断
+- 由于是右闭区间所以取不到r=nums.size()，只能取到r=nums.size()-1，看传进去 target+1 求得的下标
+*/
 
 
-// 根据给到的报错数据修改searchRange函数里的代码后，能通过，但是不能吃透为什么这种做法要自己改这么多条件。
+// 根据给到的报错数据修改searchRange函数里的代码后，能通过
 class Solution {
 public:
     int lower_bound(vector<int>& nums, int target) {
-        int l = 0, r = nums.size() - 1;
+        int l = 0, r = nums.size() - 1;//这里 r 取闭区间会导致最后一个元素取不到，比如[2,2] target=2, 传进来target+1=3，l=1不符合l<r因为r=1 于是终止循环了；如果是开区间r=2，得到 l=2或r=2 才停止
         while (l < r) {
             int mid = l + r >> 1;
             if (nums[mid] >= target) r = mid;
@@ -95,8 +97,8 @@ public:
         if (nums.empty() || start == nums.size() || nums[start] != target)
             return {-1, -1};
         int end = lower_bound(nums, target + 1);
-        if (end == nums.size() || nums[end] != target)
-            end--;
+        //判断数组最后一个元素是否为target，如果不是target，那么找该元素左边那个数, end--；如果是target，那表示是重复的数，就找它自己了不找左边的数了。
+        if (nums[end] != target)  end--;
         return {start, end};
     }
 };
