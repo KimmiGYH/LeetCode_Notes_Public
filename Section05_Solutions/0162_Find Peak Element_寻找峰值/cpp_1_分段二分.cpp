02c3286d2083036a6c235faffa03f0ce866d00e1
KimmiGYH <crazybaofulusha@163.com>
2023-05-11 19:56:41 +0800

    二分

--- Section05_Solutions/0162_Find Peak Element_寻找峰值/cpp_1_分段二分.cpp
+++ Section05_Solutions/0162_Find Peak Element_寻找峰值/cpp_1_分段二分.cpp
@@ -15,3 +15,8 @@ public:
         return r;
     }
 };
+/*nums[mid+1] 中 mid+1 是不会越界的。
+反证法：假设会越界，mid+1=nums.size()，则 mid=nums.size()-1
+因为 r=nums.size()-1，且 mid 取值是下取整，所以要使得 mid=nums.size()-1，则必须要让 l 也等于 nums.size()-1
+但 l==r 时不会进入循环，所以 mid+1 不会越界。
+ */
