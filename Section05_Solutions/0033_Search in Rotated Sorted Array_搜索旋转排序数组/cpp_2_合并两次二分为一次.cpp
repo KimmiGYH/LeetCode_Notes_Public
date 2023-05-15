40ee20e52d25db08ad771157db06019eedb25dba
KimmiGYH <crazybaofulusha@163.com>
2023-05-15 20:26:08 +0800

    二分

--- Section05_Solutions/0033_Search in Rotated Sorted Array_搜索旋转排序数组/cpp_2_合并两次二分为一次.cpp
+++ Section05_Solutions/0033_Search in Rotated Sorted Array_搜索旋转排序数组/cpp_2_合并两次二分为一次.cpp
@@ -22,8 +22,70 @@ public:
     }
 };
 
-/* 【蓝色的三种情况】：
+/* 【如果二分的位置在 target 右侧，则染成蓝色，三种情况】：
 (1) 数组元素、target 都在左侧，且 nums[i] >= target
 (2) 数组元素在右侧，target 在左侧，肯定满足蓝色
 (3) 数组元素、target都在右侧，且 nums[i] >= target
 */
+
+// 
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = -1, r = nums.size();
+        while (l + 1 < r) {
+            int mid = l + r >> 1;
+            if (nums[mid] > nums.back()) {
+                if (target > nums.back() && nums[mid] >= target) r = mid;
+                else l = mid;
+            } else {
+                if (target > nums.back() || nums[mid] >= target) r = mid;
+                else l = mid;
+            }
+        }
+        if (r == nums.size() || nums[r] != target) return -1;
+        return r;
+    }
+};
+
+// 将左开右开区间 改写成 左闭右开区间
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size();
+        while (l < r) {
+            int mid = l + r >> 1;
+            if (nums[mid] >= nums.front()) {
+                if (target >= nums.front() && nums[mid] >= target) r = mid;
+                else l = mid + 1;
+            } else {
+                if (target >= nums.front() || nums[mid] >= target) r = mid;
+                else l = mid + 1;
+            }
+        }
+        if (r == nums.size() || nums[r] != target) return -1;
+        return r;
+    }
+};
+
+// 改成 左闭右闭区间
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+        while (l < r) {
+            int mid = l + r >> 1;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] >= nums.front()) {
+                if (target >= nums.front() && nums[mid] > target) r = mid;
+                else l = mid + 1;
+            } else {
+                if (target >= nums.front() || nums[mid] > target) r = mid;
+                else l = mid + 1;
+            }
+        }
+        if (nums[r] != target) return -1;
+        return r;
+    }
+};
