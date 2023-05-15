1c429dafda47d9886d9b31ea956398254a9bb721
KimmiGYH <crazybaofulusha@163.com>
2023-05-15 21:37:47 +0800

    高精度

--- Section05_Solutions/0069_Sqrt(x)_x的平方根/cpp_1_BinarySearch.cpp
+++ Section05_Solutions/0069_Sqrt(x)_x的平方根/cpp_1_BinarySearch.cpp
@@ -5,10 +5,10 @@ using namespace std;
 class Solution {
 public:
     int mySqrt(int x) {
-        int l = 0, r = x;
+        int l = 0, r = x; //r不要取2^31-1 不然会错
         while (l < r) {
-            int mid = l + 1ll + r >> 1;
-            if (mid <= x / mid) l = mid;
+            int mid = l + 1ll + r >> 1; //l+r可能会溢出，取巧把 lll 插到中间变成 long long
+            if (mid <= x / mid) l = mid; //mid*mid会溢出，取巧把mid放到右边
             else r = mid - 1;
         }
         return l;
