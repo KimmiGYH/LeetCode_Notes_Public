47066bccc59a3aa646f7245e8eabe6a332a52d62
KimmiGYH <crazybaofulusha@163.com>
2023-05-16 07:46:47 +0800

    双指针

--- Section05_Solutions/0027_Remove Element_移除元素/cpp_1_双指针法.cpp
+++ Section05_Solutions/0027_Remove Element_移除元素/cpp_1_双指针法.cpp
@@ -1,6 +1,16 @@
-#include <iostream>
-#include <vector>
-using namespace std;
+class Solution { //快慢指针
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int slow = 0;
+        for (int fast = 0; fast < nums.size(); fast++) {
+            if (nums[fast] != val)
+                nums[slow++] = nums[fast];
+        }
+        return slow;
+    }
+};
+
+// -----------------------------------------------
 
 class Solution {
 public:
--- Section05_Solutions/0027_Remove Element_移除元素/cpp_2_逆向双指针.cpp
+++ Section05_Solutions/0027_Remove Element_移除元素/cpp_2_逆向双指针.cpp
@@ -12,3 +12,19 @@ public:
         return i;
     }
 };
+
+// ---------- 相同方法，另一种写法 2023年5月16日 -----------
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int l = 0, r = nums.size() - 1;
+        while (l <= r) {
+            if (nums[l] != val)
+                l++;
+            else
+                swap(nums[l], nums[r--]);
+        }
+        return l;
+    }
+};
