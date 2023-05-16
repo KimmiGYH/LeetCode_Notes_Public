591395c221babef245c9b31f51a0f1f731907b59
KimmiGYH <crazybaofulusha@163.com>
2023-05-16 16:02:17 +0800

    滑动窗口

--- Section05_Solutions/0003_Longest Substring Without Repeating Characters_无重复字符的最长子串/cpp_1_滑动窗口.cpp
+++ Section05_Solutions/0003_Longest Substring Without Repeating Characters_无重复字符的最长子串/cpp_1_滑动窗口.cpp
@@ -3,10 +3,11 @@ public:
     int lengthOfLongestSubstring(string s) {
         unordered_map<char, int> hash;
         int res = 0;
-        for (int i = 0, j = 0; j < s.size(); j++) {
-            hash[s[j]]++;
-            while (hash[s[j]] > 1) hash[s[i++]]--; //元素重复时，将最左边的元素移出滑窗
-            res = max(res, j - i + 1);
+        for (int i = 0, j = 0; i < s.length(); i++) {
+            hash[s[i]] ++;
+            while (hash[s[i]] > 1)
+                hash[s[j ++]] --;//元素重复时，将最左边的元素移出滑窗
+            res = max(res, i - j + 1);
         }
         return res;
     }
