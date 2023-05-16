9c6d7fbdd832d2fc8986abf3f1a613185038d8fe
KimmiGYH <crazybaofulusha@163.com>
2023-05-16 15:13:39 +0800

    滑动窗口

--- Section05_Solutions/0209_Minimum Size Subarray Sum_长度最小的子数组/cpp_1_双指针+滑动窗口_v1.cpp
+++ Section05_Solutions/0209_Minimum Size Subarray Sum_长度最小的子数组/cpp_1_双指针+滑动窗口_v1.cpp
@@ -1,19 +1,15 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-
 class Solution {
 public:
-    int minSubArrayLen(int s, vector<int>& nums) {
+    int minSubArrayLen(int target, vector<int>& nums) {
         int res = INT_MAX;
         for (int i = 0, j = 0, sum = 0; i < nums.size(); i++) {
             sum += nums[i];
-            while (sum - nums[j] >= s)  sum -= nums[j++];
-            if (sum >= s)  res = min(res, i - j + 1); // 因为是 [j, i]，所以要 + 1
+            while (sum - nums[j] >= target)
+                sum -= nums[j ++];
+            if (sum >= target)
+                res = min(res, i - j + 1);
         }
-        if (res == INT_MAX)  res = 0;
+        if (res == INT_MAX) return 0;
         return res;
     }
 };
