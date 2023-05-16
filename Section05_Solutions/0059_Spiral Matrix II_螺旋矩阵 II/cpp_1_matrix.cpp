2df9a924ab25293098b34988f58b4aa3ed7c2fe2
KimmiGYH <crazybaofulusha@163.com>
2023-05-16 18:38:27 +0800

    螺旋矩阵·方向数组

--- Section05_Solutions/0059_Spiral Matrix II_螺旋矩阵 II/cpp_1_matrix.cpp
+++ Section05_Solutions/0059_Spiral Matrix II_螺旋矩阵 II/cpp_1_matrix.cpp
@@ -1,16 +1,11 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
-
         vector<vector<int>> res(n, vector<int>(n));
-        vector<int> dx{0, 1, 0, -1}, dy{1, 0, -1, 0};
 
-        for (int k = 1, x = 0, y = 0, d = 0; k <= n * n; ++k) {
-            res[x][y] = k;
+        int dx[] = {0, 1, 0, -1}, dy[] = {1, 0, -1, 0};
+        for (int i = 1, x = 0, y = 0, d = 0; i <= n * n; i ++) {
+            res[x][y] = i;
             int a = x + dx[d], b = y + dy[d];
             if (a < 0 || a >= n || b < 0 || b >= n || res[a][b]) {
                 d = (d + 1) % 4;
@@ -21,4 +16,3 @@ public:
         return res;
     }
 };
-
