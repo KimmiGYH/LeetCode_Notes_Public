a4a47285efc3ff1c5b30e1625c243781fa414b79
KimmiGYH <crazybaofulusha@163.com>
2023-05-18 14:17:42 +0800

    链表

--- Section05_Solutions/0019_Remove Nth Node From End of List 删除链表的倒数第N个节点/cpp_1_计算链表长度.cpp
+++ Section05_Solutions/0019_Remove Nth Node From End of List 删除链表的倒数第N个节点/cpp_1_计算链表长度.cpp
@@ -25,3 +25,29 @@ public:
         return dummy->next;
     }
 };
+
+// -----自己写的代码----------
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        if (!head) return head;
+        ListNode* dummy = new ListNode(-1);;
+        dummy->next = head;
+        int size = 0;
+        for (auto p = head; p; p = p->next)
+            size ++;
+        // 要删除的是第 size-n+1 个节点，即例子中的第4个节点
+        int len = size - n; //被删节点的下标 3，第3个节点又是被删节点的前一个节点
+        ListNode* p = dummy;
+        while (--len >= 0)
+            p = p->next;
+        p->next = p->next->next;
+        return dummy->next;
+    }
+};
+
+// --- 写成 for 循环也可以 ---
+        ListNode* p = dummy;
+        for (int i = 0; i < len; i++)
+                    p = p->next;
