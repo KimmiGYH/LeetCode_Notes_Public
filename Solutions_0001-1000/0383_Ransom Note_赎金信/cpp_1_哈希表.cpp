e1a504cdcdd3cbdddd45705b4f11464dbe502a93
KimmiGYH <crazybaofulusha@163.com>
2023-05-19 20:05:55 +0800

    哈希表+数组映射

--- Solutions_0001-1000/0383_Ransom Note_赎金信/cpp_1_哈希表.cpp
+++ Solutions_0001-1000/0383_Ransom Note_赎金信/cpp_1_哈希表.cpp
@@ -11,3 +11,19 @@ public:
         return true;
     }
 };
+
+//------------自己写的---------------
+
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char, int> hr, hm;
+        for(char& c : ransomNote) hr[c]++;
+        for(char& c : magazine) hm[c]++;
+        for (char& c : ransomNote) {
+            if (hr[c] > hm[c])
+                return false;
+        }
+        return true;
+    }
+};
--- Solutions_0001-1000/0383_Ransom Note_赎金信/cpp_2_数组映射.cpp
+++ Solutions_0001-1000/0383_Ransom Note_赎金信/cpp_2_数组映射.cpp
@@ -11,3 +11,17 @@ public:
         return true;
     }
 };
+// ----------------
+
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int hash[26] = {0};
+        for (char& c : magazine) hash[c-'a']++;
+        for (char& c : ransomNote) hash[c-'a']--;
+        for (char& c : ransomNote)
+            if (hash[c-'a'] < 0)
+                return false;
+        return true;
+    }
+};
