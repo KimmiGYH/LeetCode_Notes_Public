d823a13bfd4270c0cf6abeedfc2e8364f1fa586d
KimmiGYH <crazybaofulusha@163.com>
2023-05-19 14:24:07 +0800

    删除链表

--- Section05_Solutions/0019_Remove Nth Node From End of List 删除链表的倒数第N个节点/cpp_2_快慢指针_v1.cpp
+++ Section05_Solutions/0019_Remove Nth Node From End of List 删除链表的倒数第N个节点/cpp_2_快慢指针_v1.cpp
@@ -9,15 +9,6 @@
  * };
  */
 
-// Definition for singly-linked list.
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
-
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
@@ -25,18 +16,34 @@ public:
         dummy->next = head;
         // q 快指针， p 慢指针
         ListNode *p = dummy, *q = dummy;
-
         for (int i = 0; i < n; i++) // q 先走 n 步
             q = q->next;
-        
         while (q->next) { // 一起走
             p = p->next;
-            q = q->next;
+            q = q->next;    
         }
-
         p->next = p->next->next;
-
         return dummy->next;
     }
 };
 
+// -------p 为 slow，q 为 fast-----------
+
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* dummy = new ListNode(-1);
+        dummy->next = head;
+        ListNode* fast = dummy;
+        ListNode* slow = dummy;
+        while(n--)
+            fast = fast->next;
+        while (fast->next) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+        slow->next = slow->next->next;
+        return dummy->next;
+    }
+};
