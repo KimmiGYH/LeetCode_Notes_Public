f07018e8a014a516eeb858cadb85421f3a18ce46
KimmiGYH <crazybaofulusha@163.com>
2023-05-20 18:54:54 +0800

    哈希表

--- Solutions_0001-1000/0001_Two Sum_两数之和/cpp_1_hashtable.cpp
+++ Solutions_0001-1000/0001_Two Sum_两数之和/cpp_1_hashtable.cpp
@@ -15,3 +15,23 @@ public:
         return {};
     }
 };
+
+//------错误解法❌-------
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> res;
+        unordered_map<int, int> hash;
+        for (int i = 0; i < nums.size(); i++) {
+            hash[nums[i]] = i; //这一步如果元素相同，第二个元素的下标会覆盖掉第一个元素下标
+            int val = target - nums[i];
+            if (hash.count(val) && hash[val] != i) {//导致 hash[val] != i 无法满足
+                res.push_back(hash[val]);
+                res.push_back(i);
+                return res;
+            }
+        }
+        return {};
+    }
+};
