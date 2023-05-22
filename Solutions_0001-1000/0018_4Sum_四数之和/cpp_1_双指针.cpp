0af28470a49902cc88ee118bf6ccd3af808b95f2
KimmiGYH <crazybaofulusha@163.com>
2023-05-22 09:54:03 +0800

    相向双指针

--- Solutions_0001-1000/0018_4Sum_四数之和/cpp_1_双指针.cpp
+++ Solutions_0001-1000/0018_4Sum_四数之和/cpp_1_双指针.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        int n = nums.size();
+        // nums = [-2,-1,0,0,1,2]
         vector<vector<int>> res;
+        int n = nums.size();
         sort(nums.begin(), nums.end());
-        for (int i = 0; i < n; ++i) {
-            if (i >= 1 && nums[i] == nums[i-1]) continue;
-            for (int j = i+1; j < n; ++j) {
-                if (j >= i+2 && nums[j] == nums[j-1]) continue;
-                for (int k = j+1, u = n-1; k < u; k++) {
-                    if (k >= j+2 && nums[k] == nums[k-1]) continue;
-                    while (k <= u-2 && nums[k] + nums[u] > target - nums[i] - nums[j]) u--;
-                    if (nums[k] + nums[u] == target - nums[i] - nums[j])
-                        res.push_back({nums[i], nums[j], nums[k], nums[u]});
+        for (int a = 0; a < n; a++) {
+            if (a > 0 && nums[a] == nums[a-1]) continue;
+            for (int b = a + 1; b < n; b++) {
+                if (b > a + 1 && nums[b] == nums[b-1]) continue;
+                for (int c = b + 1, d = n - 1; c < d; c++) {
+                    if (c > b + 1 && nums[c] == nums[c-1]) continue;
+                    while (c < d - 1 && (long long) nums[a] + nums[b] + nums[c] + nums[d] > target) d--;
+                    if ((long long) nums[a] + nums[b] + nums[c] + nums[d] == target)
+                        res.push_back({nums[a], nums[b], nums[c], nums[d]});
                 }
             }
         }
