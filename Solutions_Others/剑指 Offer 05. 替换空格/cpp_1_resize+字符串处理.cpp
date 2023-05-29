6ea7234f3d22bd69f99fcae93231648c8334c069
KimmiGYH <crazybaofulusha@163.com>
2023-05-29 13:42:23 +0800

    双指针

--- Solutions_Others/剑指 Offer 05. 替换空格/cpp_1_resize+字符串处理.cpp
+++ Solutions_Others/剑指 Offer 05. 替换空格/cpp_1_resize+字符串处理.cpp
@@ -19,3 +19,28 @@ public:
         return s;
     }
 };
+
+// ----------------2023年5月29日----------------
+
+class Solution {
+public:
+    string replaceSpace(string s) {
+        int cnt = 0;
+        int n = s.length();
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == ' ')
+                cnt ++;
+        }
+        s.resize(s.length() + 2 * cnt);
+        for (int i = n - 1, j = s.length() - 1; i >= 0 && j >= 0; i--) {
+            if (s[i] != ' ') {
+                s[j--] = s[i];
+            } else {
+                s[j--] = '0';
+                s[j--] = '2';
+                s[j--] = '%';
+            }
+        }
+        return s;
+    }
+};
