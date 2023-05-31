eb3486729a074b1f879c7c11216d1fe06517e6f7
KimmiGYH <crazybaofulusha@163.com>
2023-05-31 16:57:16 +0800

    栈

--- Solutions_0001-1000/0020_Valid Parentheses_有效的括号/cpp_3_栈_常规做法.cpp
+++ Solutions_0001-1000/0020_Valid Parentheses_有效的括号/cpp_3_栈_常规做法.cpp
@@ -44,3 +44,30 @@ public:
         return stk.empty();
     }
 };
+
+// -----------------2023年5月31日-------------------
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+        for (char& x : s) {
+            if (x == '(' || x == '[' || x == '{')
+                stk.push(x);
+            else if (x == ')') {
+                if (stk.empty() || stk.top() != '(')
+                    return false;
+                stk.pop();
+            } else if (x == ']') {
+                if (stk.empty() || stk.top() != '[')
+                    return false;
+                stk.pop();
+            } else {
+                if (stk.empty() || stk.top() != '{')
+                    return false;
+                stk.pop();
+            }
+        }
+        return stk.empty();
+    }
+};
