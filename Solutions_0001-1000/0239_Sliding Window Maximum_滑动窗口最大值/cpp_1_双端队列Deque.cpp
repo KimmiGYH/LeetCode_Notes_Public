01f0a350bfba0fb7a6d4ba4a306b2e94000f0905
KimmiGYH <crazybaofulusha@163.com>
2023-06-02 15:19:39 +0800

    单调队列 deque

--- Solutions_0001-1000/0239_Sliding Window Maximum_滑动窗口最大值/cpp_1_双端队列Deque.cpp
+++ Solutions_0001-1000/0239_Sliding Window Maximum_滑动窗口最大值/cpp_1_双端队列Deque.cpp
@@ -16,3 +16,28 @@ public:
         return res;
     }
 };
+
+// -------------2023年6月2日-----------------
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> q;
+        vector<int> res;
+        for (int i = 0; i < nums.size(); i++) {
+            // 过期
+            if (!q.empty() && i - q.front() + 1 > k)
+                q.pop_front();
+            
+            // nums[i] 比最后一个元素更大
+            while (!q.empty() && nums[i] > nums[q.back()])
+                q.pop_back();
+            
+            q.push_back(i);
+            
+            if (i + 1 >= k)
+                res.push_back(nums[q.front()]);
+        }
+        return res;
+    }
+};
