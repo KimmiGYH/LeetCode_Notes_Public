53563691bf21e3ee6e0a41108ef46c5a623a8aa6
KimmiGYH <crazybaofulusha@163.com>
2023-06-03 16:13:57 +0800

    优先队列 priority_queue

--- Solutions_0001-1000/0239_Sliding Window Maximum_滑动窗口最大值/cpp_2_优先队列PriorityQue.cpp
+++ Solutions_0001-1000/0239_Sliding Window Maximum_滑动窗口最大值/cpp_2_优先队列PriorityQue.cpp
@@ -8,9 +8,11 @@ public:
         vector<int> res;
         priority_queue<pair<int, int>> q;
         for (int i = 0; i < nums.size(); ++i) {
-            while (!q.empty() && q.top().second <= i - k) q.pop();
+            while (!q.empty() && q.top().second <= i - k)
+                q.pop();
             q.push({nums[i], i});
-            if (i >= k - 1) res.push_back(q.top().first);
+            if (i >= k - 1)
+                res.push_back(q.top().first);
         }
         return res;
     }
