46ffb7cff977cc196c04d4059f4d8ff99e5c9f17
KimmiGYH <crazybaofulusha@163.com>
2023-06-05 21:49:06 +0800

    二叉树层序遍历

--- Solutions_0001-1000/0102_Binary Tree Level Order Traversal_二叉树的层序遍历/cpp_1_层序_BFS_v1.cpp
+++ Solutions_0001-1000/0102_Binary Tree Level Order Traversal_二叉树的层序遍历/cpp_1_层序_BFS_v1.cpp
@@ -35,3 +35,39 @@ public:
         return res;
     }
 };
+
+// ------------2023年6月5日--------------
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+        queue<TreeNode*> q;
+        if (root)
+            q.push(root);
+        while (!q.empty()) {
+            vector<int> ans;
+            int sz = q.size();
+            while (sz --) {
+                auto t = q.front();
+                q.pop();
+                ans.push_back(t->val);
+                if (t->left) q.push(t->left);
+                if (t->right) q.push(t->right);
+            }
+            res.push_back(ans);
+        }
+        return res;
+    }
+};
