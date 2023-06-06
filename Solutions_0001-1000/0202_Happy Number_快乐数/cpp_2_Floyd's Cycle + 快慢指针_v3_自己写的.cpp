6a980e3305dc0fb44f6717af7a336d13ed50772b
KimmiGYH <crazybaofulusha@163.com>
2023-06-06 21:18:24 +0800

    快慢指针

---
+++ Solutions_0001-1000/0202_Happy Number_快乐数/cpp_2_Floyd's Cycle + 快慢指针_v3_自己写的.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int get(int n) {
+        int a = 0, sum = 0;
+        while (n) {
+            a = n % 10;
+            n /= 10;
+            sum += a * a;
+        }
+        return sum;
+    }
+
+    bool isHappy(int n) {
+        int slow = get(n);
+        int fast = get(get(n));
+        while (true) {
+            if (fast == 1 || slow == 1)
+                return true;
+            else if (fast != slow) {
+                fast = get(get(fast));
+                slow = get(slow);
+            } else if (fast == slow)
+                break;
+        }
+        return false;
+    }
+};
