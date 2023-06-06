c830a8f902f86dd7d29d5f1177b6e5a40c88b351
KimmiGYH <crazybaofulusha@163.com>
2023-06-06 10:16:55 +0800

    二叉树层序遍历

---
+++ Solutions_0001-1000/0199_Binary Tree Right Side View_二叉树的右视图/cpp_1_层序遍历_自己写的.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> res;
+        vector<vector<int>> total;
+        queue<TreeNode*> q;
+        if (root) q.push(root);
+        while (!q.empty()) {
+            vector<int> ans;
+            int sz = q.size();
+            while (sz--) {
+                auto t = q.front();
+                q.pop();
+                ans.push_back(t->val);
+                if (t->left) q.push(t->left);
+                if (t->right) q.push(t->right);
+            }
+            total.push_back(ans);
+        }
+        for (auto x : total)
+            res.push_back(x.back());
+        return res;
+    }
+};
