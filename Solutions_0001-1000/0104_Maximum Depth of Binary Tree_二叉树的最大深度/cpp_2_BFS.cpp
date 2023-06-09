8bbff4296d32f686f292127da0c43807a1dad309
KimmiGYH <crazybaofulusha@163.com>
2023-06-09 11:21:49 +0800

    二叉树最大深度

---
+++ Solutions_0001-1000/0104_Maximum Depth of Binary Tree_二叉树的最大深度/cpp_2_BFS.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        int depth = 0;
+        queue<TreeNode*> q;
+        if (root) q.push(root);
+        while (!q.empty()) {
+            int sz = q.size();
+            while (sz--) {
+                auto t = q.front();
+                q.pop();
+                if (t->left) q.push(t->left);
+                if (t->right) q.push(t->right);
+            }
+            depth ++;
+        }
+        return depth;
+    }
+};
