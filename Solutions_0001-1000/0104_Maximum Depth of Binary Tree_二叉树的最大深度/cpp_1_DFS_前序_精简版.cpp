4a5b3509dc72fece054dfb0605e457c007e1fcc2
KimmiGYH <crazybaofulusha@163.com>
2023-06-13 17:08:21 +0800

    DFS前序求最大深度

---
+++ Solutions_0001-1000/0104_Maximum Depth of Binary Tree_二叉树的最大深度/cpp_1_DFS_前序_精简版.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int result;
+
+    void getDepth(TreeNode* node, int depth) {
+        result = depth > result ? depth : result; // 中
+        if (!node->left && !node->right)
+            return;
+        if (node->left)
+            getDepth(node->left, depth + 1);
+        if (node->right)
+            getDepth(node->right, depth + 1);
+        return;
+    }
+
+    int maxDepth(TreeNode* root) {
+        result = 0;
+        if (!root) return result;
+        getDepth(root, 1);
+        return result;
+    }
+};
