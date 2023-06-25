7a3b86d6ac9ef95a3e60d8501f17cd2af289d703
KimmiGYH <crazybaofulusha@163.com>
2023-06-25 21:58:10 +0800

    路径总和+DFS回溯

--- Solutions_0001-1000/0113_Path Sum II_路径总和 II/cpp_1_DFS＋回溯_正向_自己写的_optimal.cpp
+++ Solutions_0001-1000/0113_Path Sum II_路径总和 II/cpp_1_DFS＋回溯_正向_自己写的_optimal.cpp
@@ -33,3 +33,40 @@ public:
         return res;
     }
 };
+
+// ------------2023年6月25日----------
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> path;
+    vector<vector<int>> res;
+
+    void dfs(TreeNode* root, int& targetSum, int sum) {
+        path.push_back(root->val);
+        if (!root->left && !root->right)
+            if (sum == targetSum)
+                res.push_back(path);
+        if (root->left)
+            dfs(root->left, targetSum, sum + root->left->val);
+        if (root->right)
+            dfs(root->right, targetSum, sum + root->right->val);
+        path.pop_back();
+    }
+
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        if (!root) return res;
+        dfs(root, targetSum, root->val);
+        return res;
+    }
+};
