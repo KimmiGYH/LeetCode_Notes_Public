c1f3fc9e358dccf1c5016d1e7a4846ce9ea2394f
KimmiGYH <crazybaofulusha@163.com>
2023-06-25 23:33:29 +0800

    构造二叉树＋递归DFS

--- Solutions_0001-1000/0106_Construct Binary Tree from Inorder and Postorder Traversal_从中序与后序遍历序列构造二叉树/cpp_1_DFS.cpp
+++ Solutions_0001-1000/0106_Construct Binary Tree from Inorder and Postorder Traversal_从中序与后序遍历序列构造二叉树/cpp_1_DFS.cpp
@@ -28,3 +28,37 @@ public:
         return dfs(inorder, postorder, 0, inorder.size()-1, 0, postorder.size()-1);
     }
 };
+
+// -------------2023年6月25日---------------
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    unordered_map<int, int> hash;
+
+    TreeNode* build(vector<int>& inorder, vector<int>& postorder, int il, int ir, int pl, int pr) {
+        if (il > ir) return NULL;
+        TreeNode* root = new TreeNode(postorder[pr]);
+        int k = hash[root->val];
+        root->left = build(inorder, postorder, il, k-1, pl, pl + k-1 - il);
+        root->right = build(inorder, postorder, k+1, ir, pl + k-1 - il + 1, pr - 1);
+        return root;
+    }
+
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        for (int i = 0; i < inorder.size(); i++) {
+            hash[inorder[i]] = i;
+        }
+        return build(inorder, postorder, 0, inorder.size()-1, 0, postorder.size()-1);
+    }
+};
