774c24ae5de90a6ba080072be0d99a567256e420
KimmiGYH <crazybaofulusha@163.com>
2023-06-25 15:53:48 +0800

    二叉树深度，DFS+回溯、BFS

--- Solutions_0001-1000/0513_Find Bottom Left Tree Value_找树左下角的值/cpp_1_BFS_迭代_自己写的.cpp
+++ Solutions_0001-1000/0513_Find Bottom Left Tree Value_找树左下角的值/cpp_1_BFS_迭代_自己写的.cpp
@@ -31,3 +31,40 @@ public:
         return ans.back().back();
     }
 };
+
+// -------2023年6月25日----------
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    queue<TreeNode*> q;
+    int res = 0;
+
+    int findBottomLeftValue(TreeNode* root) {
+        if (!root) return 0;
+        q.push(root);
+        while (!q.empty()) {
+            vector<int> vec;
+            int sz = q.size();
+            while (sz--) {
+                auto t = q.front();
+                q.pop();
+                vec.push_back(t->val);
+                if (t->left) q.push(t->left);
+                if (t->right) q.push(t->right);
+            }
+            res = vec[0];
+        }
+        return res;
+    }
+};
