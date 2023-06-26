0a6e1b560b4156990ce9c450083a19609883ef33
KimmiGYH <crazybaofulusha@163.com>
2023-06-26 22:01:52 +0800

    中序遍历，迭代stack

---
+++ Solutions_0001-1000/0098_Validate Binary Search Tree_验证二叉搜索树/cpp_4_迭代_中序_stack.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        TreeNode* pre = nullptr;
+        stack<TreeNode*> stk;
+        while (root || !stk.empty()) {
+            if (root) {
+                stk.push(root);
+                root = root->left;
+            } else {
+                root = stk.top();
+                stk.pop();
+                if (pre && root->val <= pre->val)
+                    return false;
+                pre = root;
+                root = root->right;
+            }
+        }
+        return true;
+    }
+};
