fe625e0f6aa64d0f2b3080a0ca9dc8970afd78cc
KimmiGYH <crazybaofulusha@163.com>
2023-06-27 18:16:44 +0800

    二叉搜索树，最近公共祖先，递归DFS，后序

---
+++ Solutions_0001-1000/0235_Lowest Common Ancestor of a Binary Search Tree_二叉搜索树的最近公共祖先/cpp_1_DFS_v4_不写swap.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root || p == root || q == root)
+            return root;
+        if (p->val < root->val && q->val < root->val)
+            return lowestCommonAncestor(root->left, p, q);
+        if (p->val > root->val && q->val > root->val)
+            return lowestCommonAncestor(root->right, p, q);
+        return root;
+    }
+};
+
+// ----------- 注意 ------------
+// 以下写法错误
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root || p == root || q == root)
+            return root;
+        if (p->val < root->val && q->val > root->val)
+            return root; //这样写当 p->val 大于 q->val 时是错的
+        if (p->val < root->val && q->val < root->val)
+            return lowestCommonAncestor(root->left, p, q);
+        if (p->val > root->val && q->val > root->val)
+            return lowestCommonAncestor(root->right, p, q);
+        return NULL;
+    }
+};
