e4858e397b1af94957ca226a6f89f1af06be498c
KimmiGYH <crazybaofulusha@163.com>
2023-06-27 17:17:28 +0800

    最近公共祖先，递归DFS，后序

--- Solutions_0001-1000/0236_Lowest Common Ancestor of a Binary Tree_二叉树的最近公共祖先/cpp_1_LCA_DFS.cpp
+++ Solutions_0001-1000/0236_Lowest Common Ancestor of a Binary Tree_二叉树的最近公共祖先/cpp_1_LCA_DFS.cpp
@@ -11,8 +11,9 @@ class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if (!root || root == p || root == q)  return root;
-        TreeNode* left = lowestCommonAncestor(root->left, p, q);
-        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        TreeNode* left = lowestCommonAncestor(root->left, p, q); //左
+        TreeNode* right = lowestCommonAncestor(root->right, p, q); //右
+        //中
         if (!left && !right)  return nullptr; // 1. left 和 right 同时为空
         if (!left)  return right; // 3. left 为空，right 不为空
         if (!right)  return left; // 4. right 为空，left 不为空
