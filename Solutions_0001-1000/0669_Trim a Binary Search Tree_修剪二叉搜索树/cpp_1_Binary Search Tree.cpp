3a7fd6f849c299ae02a074d7751a046e3948b0d9
KimmiGYH <crazybaofulusha@163.com>
2023-06-30 16:35:28 +0800

    删除二叉搜索树中多个节点，递归

--- Solutions_0001-1000/0669_Trim a Binary Search Tree_修剪二叉搜索树/cpp_1_Binary Search Tree.cpp
+++ Solutions_0001-1000/0669_Trim a Binary Search Tree_修剪二叉搜索树/cpp_1_Binary Search Tree.cpp
@@ -15,12 +15,31 @@ struct TreeNode {
 class Solution {
 public:
     TreeNode* trimBST(TreeNode* root, int low, int high) {
-        // 二叉搜索树的性质：左 ≤ 根 ≤ 右
         if (!root) return nullptr;
-        if (root->val < low) return trimBST(root->right, low, high);
-        if (root->val > high) return trimBST(root->left, low, high);
+        if (root->val < low)
+            return trimBST(root->right, low, high);
+        if (root->val > high)
+            return trimBST(root->left, low, high);
         root->left = trimBST(root->left, low, high);
         root->right = trimBST(root->right, low, high);
+        return root;
+    }
+};
+
+// --------2023年6月30日--------
+
+class Solution {
+public:
+    TreeNode* trimBST(TreeNode* root, int low, int high) {
+        if (!root) return nullptr;
+        if (root->val < low)
+            return trimBST(root->right, low, high);
+        else if (root->val > high)
+            return trimBST(root->left, low, high);
+        else {
+            root->left = trimBST(root->left, low, high);
+            root->right = trimBST(root->right, low, high);
+        }
         return root;
     }
 };
