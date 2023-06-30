9a1195f73904505adb7398d173e2f3f42f60c425
KimmiGYH <crazybaofulusha@163.com>
2023-06-30 21:36:23 +0800

    二叉搜索树逆中序遍历，递归＋迭代

---
+++ Solutions_0001-1000/0538_Convert BST to Greater Tree_把二叉搜索树转换为累加树/cpp_2_BST_迭代.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* convertBST(TreeNode* root) {
+        if (!root) return nullptr;
+        stack<TreeNode*> stk;
+        int sum = 0;
+        TreeNode* cur = root;
+        while (cur || !stk.empty()) {
+            if (cur) {
+                stk.push(cur);
+                cur = cur->right;
+            } else {
+                cur = stk.top();
+                stk.pop();
+                int x = cur->val;
+                cur->val += sum;
+                sum += x;
+                cur = cur->left;
+            }
+        }
+        return root;
+    }
+};
