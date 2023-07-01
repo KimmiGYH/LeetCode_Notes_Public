f1321db33d68d916735246fcbc9cce2b380a4ce4
KimmiGYH <crazybaofulusha@163.com>
2023-07-01 12:55:38 +0800

    组合＋DFS＋回溯

--- Solutions_0001-1000/0077_Combinations_组合/cpp_1_DFS_Backtracking.cpp
+++ Solutions_0001-1000/0077_Combinations_组合/cpp_1_DFS_Backtracking.cpp
@@ -20,11 +20,35 @@ public:
             res.push_back(path);
             return;
         }
-
         for (int i = start; i <= n; i++) {
             path.push_back(i);
             dfs(n, k - 1, i + 1);
             path.pop_back();
         }
+    }
+};
+
+// --------- 2023年7月1日 另种写法----------
+
+class Solution {
+public:
+    vector<vector<int>> res;
+    vector<int> path;
+
+    void backtracking(int& n, int& k, int startIndex) {
+        if (path.size() == k) {
+            res.push_back(path);
+            return;
+        }
+        for (int i = startIndex; i <= n; i++) {
+            path.push_back(i);
+            backtracking(n, k, i + 1);
+            path.pop_back();
+        }
+    }
+
+    vector<vector<int>> combine(int n, int k) {
+        backtracking(n, k, 1);
+        return res;
     }
 };
