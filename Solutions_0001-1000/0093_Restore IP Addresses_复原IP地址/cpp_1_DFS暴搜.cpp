e17acc5841c9779a30944ac8430411e6ceb47bcf
KimmiGYH <crazybaofulusha@163.com>
2023-07-17 16:43:56 +0800

    分割＋DFS＋回溯

--- Solutions_0001-1000/0093_Restore IP Addresses_复原IP地址/cpp_1_DFS暴搜.cpp
+++ Solutions_0001-1000/0093_Restore IP Addresses_复原IP地址/cpp_1_DFS暴搜.cpp
@@ -11,7 +11,7 @@ public:
         return ans;        
     }
 
-    // u 表示枚举到的字符串下标，k 表示当前搜到第几个数，path 表示当前的方案
+    // u 表示枚举到的字符串下标，k 表示当前搜到第几组数，path 表示当前的方案
     void dfs(string& s, int u, int k, string path) {
         if (u == s.size()) {
             if (k == 4) {
@@ -20,13 +20,13 @@ public:
             }
             return;
         }
-        if (k > 4)  return;
-        // t 表示当前这个数的值是多少
-        for (int i = u, t = 0; i < s.size(); i++) {
-            // 有两位 或者 有前导0，则 break
-            if (i > u && s[u] == '0')  break;
-            t = t * 10 + s[i] - '0';
-            if (t <= 255) dfs(s, i + 1, k + 1, path + to_string(t) + '.') ;
+        if (k == 4)  return; // 表示还没结束时 当前方案至少会有五组数
+        // num 表示当前这个数的值是多少
+        for (int i = u, num = 0; i < s.size(); i++) {
+            if (i > u && s[u] == '0')
+                break; // 有两位且有前导0，则 break
+            num = num * 10 + s[i] - '0';
+            if (num <= 255) dfs(s, i + 1, k + 1, path + to_string(num) + '.') ;
             else break;
         }
     }
