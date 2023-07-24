ac9f5427dad1c20504d76869d204d4f75390cc10
KimmiGYH <crazybaofulusha@163.com>
2023-07-24 12:15:12 +0800

    回溯＋哈希表

--- Solutions_0001-1000/0491_Non-decreasing Subsequences_递增子序列/cpp_1_回溯_哈希表.cpp
+++ Solutions_0001-1000/0491_Non-decreasing Subsequences_递增子序列/cpp_1_回溯_哈希表.cpp
@@ -25,3 +25,32 @@ public:
         return res;
     }
 };
+
+// -----------2023年7月24日-----------
+
+class Solution {
+public:
+    vector<int> path;
+    vector<vector<int>> res;
+    
+    void backtracking(vector<int>& nums, int startIndex) {
+        if (path.size() >= 2)
+            res.push_back(path);
+        unordered_set<int> S;
+        for (int i = startIndex; i < nums.size(); i++) {
+            if (S.find(nums[i]) != S.end())
+                continue;
+            S.insert(nums[i]);
+            if (!path.empty() && nums[i] < path.back())
+                    continue;
+            path.push_back(nums[i]);
+            backtracking(nums, i + 1);
+            path.pop_back();
+        }
+    }
+
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
+        backtracking(nums, 0);
+        return res;
+    }
+};
